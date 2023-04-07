22611734b86ac3e949bdb9c3beed9235c1947981
Rémi Louf <remi@thetypicalset.com>
2023-04-07 20:45:54 +0200

    s/compose/render

--- outlines/__init__.py
+++ outlines/__init__.py
@@ -31,7 +31,7 @@ References
 from outlines.function import fn
 from outlines.image import as_image
 from outlines.program import chain, program
-from outlines.text import as_string, compose
+from outlines.text import as_string, render
 
 __all__ = [
     "chain",
@@ -39,5 +39,5 @@ __all__ = [
     "as_string",
     "fn",
     "program",
-    "compose",
+    "render",
 ]
--- tests/text/test_compose.py
+++ tests/text/test_compose.py
@@ -1,40 +1,40 @@
 import pytest
 
-from outlines.text import compose, string
+from outlines.text import render, string
 from outlines.text.basic import Add
 from outlines.text.var import StringConstant, StringVariable
 
 
 def test_template_text():
     with pytest.raises(NameError):
-        compose("String ${one}", two="two")
+        render("String ${one}", two="two")
 
-    t = compose("Test")
+    t = render("Test")
     assert t == "Test"
 
-    t = compose("Test ${variable}", variable="string")
+    t = render("Test ${variable}", variable="string")
     assert t == "Test string"
 
-    t = compose("Test ${variable}", variable=1)
+    t = render("Test ${variable}", variable=1)
     assert t == "Test 1"
 
-    t = compose("Test repeated ${variable} ${variable}", variable="string")
+    t = render("Test repeated ${variable} ${variable}", variable="string")
     assert t == "Test repeated string string"
 
-    t = compose("Test ${one} ${two}", one="1", two="2")
+    t = render("Test ${one} ${two}", one="1", two="2")
     assert t == "Test 1 2"
 
 
 def test_template_string_variable():
     variable = string()
-    t = compose("Test ${variable}", variable=variable)
+    t = render("Test ${variable}", variable=variable)
     assert isinstance(t.owner.op, Add)
     assert isinstance(t.owner.inputs[0], StringConstant)
     assert isinstance(t.owner.inputs[1], StringVariable)
     assert t.owner.inputs[0].value == "Test "
 
     variable = string()
-    t = compose("${variable} test", variable=variable)
+    t = render("${variable} test", variable=variable)
     assert isinstance(t.owner.op, Add)
     assert isinstance(t.owner.inputs[0], StringVariable)
     assert isinstance(t.owner.inputs[1], StringConstant)
@@ -44,7 +44,7 @@ def test_template_string_variable():
 def test_template_few_shots():
     wa = string()
     examples = [["here", "there"], ["this", "that"]]
-    prompt = compose(
+    prompt = render(
         """
         This is a test
 
