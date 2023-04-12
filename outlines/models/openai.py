4ffc330948ca19415bd4b5aefad00d4595ed147e
Rémi Louf <remi@thetypicalset.com>
2023-04-12 22:59:54 +0200

    Refactor the OpenAI connector

--- outlines/models/openai.py
+++ outlines/models/openai.py
@@ -1,77 +1,101 @@
+import functools
 import os
-from typing import List, Optional
-
-try:
-    import openai
-    from openai import error
-except ImportError:
-    raise ImportError("You need to install `openai` to run OpenAI's language models.")
+from typing import Callable, List, Optional, Tuple
 
 
-class OpenAI:
-    """Represents any of OpenAI's language models
+def OpenAICompletion(
+    model_name: str,
+    stop_at: Optional[List[str]] = None,
+    max_tokens: Optional[int] = None,
+    temperature: Optional[float] = None,
+) -> Callable:
+    """Create a function that will call the OpenAI API.
 
-    You should have the `openai` package installed, and store you OpenAI key in
-    the `OPENAI_API_KEY` environment variable.
+    You should have the `openai` package installed. Available models are listed
+    in the `OpenAI documentation <https://platform.openai.com/docs/models/overview>`_.
+
+    Parameters
+    ----------
+    model_name: str
+        The name of the model as listed in the OpenAI documentation.
+    stop_at
+        A list of tokens which, when found, stop the generation.
+    max_tokens
+        The maximum number of tokens to generate.
+    temperature
+        Value used to module the next token probabilities.
+
+    Returns
+    -------
+    A function that will call OpenAI's API with the given parameters when passed
+    a prompt.
 
     """
+    import openai
 
-    def __init__(
-        self,
-        model: str,
-        stops_at: Optional[List[str]] = None,
-        max_tokens: Optional[int] = None,
-        temperature: Optional[float] = None,
-    ):
-        """Initialize the OpenAI model."""
+    try:
+        os.environ["OPENAI_API_KEY"]
+    except KeyError:
+        raise OSError(
+            "Could not find the `OPENAI_API_KEY` environment variable, which is necessary to call "
+            "OpenAI's APIs. Please make sure it is set before re-running your model."
+        )
 
+    parameters = validate_parameters(stop_at, max_tokens, temperature)
+
+    def call(prompt: str) -> str:
         try:
-            self.openai_api_key = os.environ["OPENAI_API_KEY"]
-        except KeyError:
-            raise OSError(
-                "Could not find the `OPENAI_API_KEY` environment variable. Please make sure it is set to your OpenAI key before re-running your model."
-            )
+            result = call_completion_api(model_name, prompt, *parameters)
+            return result
+        except (
+            openai.error.RateLimitError,
+            openai.error.Timeout,
+            openai.error.TryAgain,
+            openai.error.APIConnectionError,
+            openai.error.ServiceUnavailableError,
+        ) as e:
+            raise OSError(f"Could not connect to the OpenAI API: {e}")
+        except (
+            openai.error.AuthenticationError,
+            openai.error.PermissionError,
+            openai.error.InvalidRequestError,
+        ) as e:
+            raise e
 
-        available_models = openai.Model.list()
-        available_model_names = [model["id"] for model in available_models["data"]]
-        if model not in available_model_names:
-            raise OSError(f"{model} is not a valid OpenAI model name.")
-        self.model = model
+    return call
 
-        if stops_at is not None and len(stops_at) > 4:
-            raise Exception("OpenAI's API does not accept more than 4 stop sequences.")
-        self.stops_at = stops_at
 
-        if max_tokens is None:
-            max_tokens = 216
-        self.max_tokens = max_tokens
+@functools.lru_cache
+def call_completion_api(
+    model: str,
+    prompt: str,
+    stop_sequences: Tuple[str],
+    max_tokens: int,
+    temperature: float,
+):
+    import openai
 
-        if temperature is None:
-            temperature = 1.0
-        self.temperature = temperature
+    response = openai.Completion.create(
+        engine=model,
+        prompt=prompt,
+        temperature=temperature,
+        max_tokens=max_tokens,
+        stop=stop_sequences,
+    )
 
-    def __call__(self, prompt: str) -> str:
-        try:
-            resp = openai.Completion.create(
-                model=self.model,
-                prompt=prompt,
-                max_tokens=self.max_tokens,
-                stop=self.stops_at,
-                temperature=self.temperature,
-            )
-        except error.APIConnectionError as e:
-            raise OSError(f"Open API failed to connect: {e}")
-        except error.AuthenticationError as e:
-            raise OSError(
-                f"Open API request not authorized: {e}. Check that the token provided is valid."
-            )
-        except error.PermissionError as e:
-            raise OSError(f"Open API request was not permitted: {e}")
-        except error.RateLimitError as e:
-            raise OSError(
-                f"Open API requests exceeded the rate limit: {e}. Wait before re-running your program."
-            )
-        except error.Timeout as e:
-            raise OSError(f"Open API request timed out: {e}")
+    return response["choices"][0]["text"]
 
-        return resp["choices"][0]["text"]
+
+def validate_parameters(
+    stop_at, max_tokens, temperature
+) -> Tuple[Tuple[str], int, float]:
+    if stop_at is not None and len(stop_at) > 4:
+        raise TypeError("OpenAI's API does not accept more than 4 stop sequences.")
+    elif stop_at is not None:
+        stop_at = tuple(stop_at)
+    if max_tokens is None:
+        max_tokens = 216
+    if temperature is None:
+        temperature = 1.0
+
+    return stop_at, max_tokens, temperature
