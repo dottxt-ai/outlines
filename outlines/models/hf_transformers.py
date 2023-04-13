1d330e2a2b92281218209b7ba62281feabf9a3a2
Rémi Louf <remilouf@gmail.com>
2023-04-13 15:09:44 +0200

    Refactor the HuggingFace `transformers` connector

--- outlines/models/hf_transformers.py
+++ outlines/models/hf_transformers.py
@@ -1,94 +1,75 @@
-from typing import Optional
-
-try:
-    import torch
-    from transformers import AutoModelForCausalLM, AutoTokenizer
-except ImportError:
-    raise ImportError(
-        "You need to install `transformers` and `torch` to run HuggingFace's Causal LM models."
-    )
+import functools
+from typing import Callable, Optional
 
 
-class HFCausalLM:
-    """Represent any of HuggingFace's causal language model implementations.
+def HuggingFaceCompletion(
+    model_name: str,
+    max_tokens: Optional[int] = None,
+    temperature: Optional[float] = None,
+) -> Callable:
+    """Create a function that will call the `generate` method of a `transformers` model.
 
     You should have the `torch` and `transformers` packages installed. First
     execution may take a while since the pre-trained weights will be downloaded.
+    Available models are listed on `HuggingFace's model page <https://huggingface.co/models>`_.
 
-    Available models are listed on https://huggingface.co/models
+    Note
+    ----
 
-    Example
-    ------
+    To my knowledge `tranformers` does not simply allow to stop the generation
+    after a given sequence has been generated. We will need to implement this
+    manually for this integration to have the same features as `OpenAICompletion`.
 
-    >> from outlines.text.models import HFCausalLM
-    >> from outlines.text import string
-    >>
-    >> gpt2 = HFCausalLM("gpt2")
-    >> in = string()
-    >> out = gpt2(in)
-
-    Attributes
+    Parameters
     ----------
-    model_name
-        The model string identifier in the `transformers` library.
+    model_name: str
+        The name of the model as listed on HuggingFace's models page.
+    max_tokens
+        The maximum number of tokens to generate.
+    temperature
+        Value used to module the next token probabilities.
+
+    Returns
+    -------
+    A function that will generate tokens from the model when passed a prompt.
 
     """
+    if max_tokens is None:
+        max_tokens = 216
 
-    def __init__(
-        self,
-        model: str,
-        max_tokens: Optional[int] = None,
-        temperature: Optional[float] = None,
-    ):
-        """Instantiate the model `Op`.
+    if temperature is None:
+        temperature = 1.0
 
-        Parameters
-        ----------
-        model
-            The model id of a model hosted inside a model repo on huggingface.co
+    def call(prompt: str) -> str:
+        return call_model_generate_method(model_name, prompt, max_tokens, temperature)
 
-        """
-        if max_tokens is None:
-            max_tokens = 216
-        self.max_tokens = max_tokens
+    return call
 
-        if temperature is None:
-            temperature = 1.0
-        self.temperature = temperature
 
-        self.model_name = model
+@functools.lru_cache
+def call_model_generate_method(
+    model_name: str, prompt: str, max_tokens: int, temperature: float
+) -> str:
+    import torch
+    from transformers import AutoModelForCausalLM, AutoTokenizer
 
-    def __call__(self, prompt: str) -> str:
-        """Sample new tokens give the tokenized prompt.
+    tokenizer = AutoTokenizer.from_pretrained(model_name)
+    model = AutoModelForCausalLM.from_pretrained(model_name)
 
-        Since HuggingFace's `generate` method returns the prompt along with the
-        generated token we need to truncate the returned array of tokens.
+    prompt_tokens = tokenizer(prompt, return_tensors="pt")
 
-        Parameters
-        ----------
-        prompt_tokens
-            A dictionary that contains the ids of the tokens contained in the input
-            prompt and the input mask. This is the default output of HuggingFace's
-            tokenizers.
+    if torch.cuda.is_available():
+        model = model.to("cuda")
+        prompt_tokens = prompt_tokens.to("cuda")
 
-        """
-        tokenizer = AutoTokenizer.from_pretrained(self.model_name)
-        model = AutoModelForCausalLM.from_pretrained(self.model_name)
+    returned_tokens = model.generate(
+        **prompt_tokens,
+        do_sample=True,
+        temperature=temperature,
+        max_new_tokens=max_tokens,
+        pad_token_id=tokenizer.eos_token_id,
+    )
+    new_tokens = returned_tokens[:, prompt_tokens["input_ids"].shape[1] + 1 :]
+    new_tokens = new_tokens.squeeze()
 
-        prompt_tokens = tokenizer(prompt, return_tensors="pt")
-
-        if torch.cuda.is_available():
-            model = model.to("cuda")
-            prompt_tokens = prompt_tokens.to("cuda")
-
-        returned_tokens = model.generate(
-            **prompt_tokens,
-            do_sample=True,
-            temperature=self.temperature,
-            max_new_tokens=self.max_tokens,
-            pad_token_id=tokenizer.eos_token_id,
-        )
-        new_tokens = returned_tokens[:, prompt_tokens["input_ids"].shape[1] + 1 :]
-        new_tokens = new_tokens.squeeze()
-
-        return tokenizer.decode(new_tokens, skip_special_tokens=True)
+    return tokenizer.decode(new_tokens, skip_special_tokens=True)
