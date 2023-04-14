99adac47e82774515a72c8b05dff02d5e00f5bf7
Rémi Louf <remi@thetypicalset.com>
2023-04-14 11:55:13 +0200

    Cache model calls using `joblib`

---
+++ outlines/cache.py
@@ -0,0 +1,51 @@
+import os
+
+import joblib
+
+home_dir = os.path.expanduser("~")
+cache_dir = os.environ.get("OUTLINES_CACHE_DIR", f"{home_dir}/.cache/outlines")
+memory = joblib.Memory(cache_dir, verbose=0)  # type: ignore[attr-defined]
+
+
+def get():
+    """Get the context object that contains previously-computed return values.
+
+    The cache is used to avoid unnecessary computations and API calls, which can
+    be long and expensive for large models.
+
+    The cache directory defaults to `HOMEDIR/.cache/outlines`, but this choice
+    can be overriden by the user by setting the value of the `OUTLINES_CACHE_DIR`
+    environment variable.
+
+    """
+    return memory
+
+
+def disable():
+    """Disable the cache for this session.
+
+    Generative models output different results each time they are called when
+    sampling. This can be a desirable property for some workflows, in which case
+    one can call `outlines.call.disable` to disable the cache for the session.
+
+    This function does not delete the cache, call `outlines.cache.clear`
+    instead. It also does not overwrite the cache with the values returned
+    during the session.
+
+    Example
+    -------
+
+    `outlines.cache.disable` should be called right after importing outlines:
+
+    >>> import outlines.cache as cache
+    >>> cache.disable()
+
+    """
+    global memory
+    memory = joblib.Memory(None)
+
+
+def clear():
+    """Erase the cache completely."""
+    cache = get()
+    cache.clear()
--- outlines/models/hf_diffusers.py
+++ outlines/models/hf_diffusers.py
@@ -1,6 +1,9 @@
-import functools
-
+"""Integration with HuggingFace's `diffusers` library."""
 from PIL.Image import Image as PILImage
+
+import outlines.cache as cache
+
+memory = cache.get()
 
 
 def HuggingFaceDiffuser(model_name: str) -> PILImage:
@@ -13,14 +16,19 @@ def HuggingFaceDiffuser(model_name: str) -> PILImage:
 
     """
 
-    @functools.lru_cache
     def call(prompt: str) -> str:
-        import torch
-        from diffusers import StableDiffusionPipeline
+        return call_stable_diffusion_pipeline(model_name, prompt)
 
-        pipe = StableDiffusionPipeline.from_pretrained(model_name)
-        if torch.cuda.is_available():
-            pipe = pipe.to("cuda")
-        image = pipe(prompt).images[0]
 
-        return image
+@memory.cache
+def call_stable_diffusion_pipeline(model_name: str, prompt: str) -> PILImage:
+    """Build and call the Stable Diffusion pipeline."""
+    import torch
+    from diffusers import StableDiffusionPipeline
+
+    pipe = StableDiffusionPipeline.from_pretrained(model_name)
+    if torch.cuda.is_available():
+        pipe = pipe.to("cuda")
+    image = pipe(prompt).images[0]
+
+    return image
