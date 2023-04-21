0693ba6b396fe8ed6c64a8b5e65fc6a75b63e41d
Rémi Louf <remi@thetypicalset.com>
2023-04-21 16:40:16 +0200

    Add the `Tool` class and `tool` decorator

--- outlines/__init__.py
+++ outlines/__init__.py
@@ -1,10 +1,12 @@
 """Outlines is a Generative Model Programming Framework."""
 from outlines.image import generation
 from outlines.text import completion, prompt, render
+from outlines.tools import tool
 
 __all__ = [
     "completion",
     "generation",
     "prompt",
     "render",
+    "tool",
 ]
