8161b70d4f24a70be81a68bbc863137f13980f3c
Rémi Louf <remi@thetypicalset.com>
2023-05-01 13:29:41 +0200

    Generate several samples with HF models

--- outlines/models/hf_transformers.py
+++ outlines/models/hf_transformers.py
@@ -1,5 +1,5 @@
 """Integration with HuggingFace's `transformers` library."""
-from typing import Callable, Optional
+from typing import Callable, List, Optional, Union
 
 import outlines.cache as cache
 
@@ -8,8 +8,8 @@ memory = cache.get()
 
 def HuggingFaceCompletion(
     model_name: str,
-    max_tokens: Optional[int] = None,
-    temperature: Optional[float] = None,
+    max_tokens: Optional[int] = 216,
+    temperature: Optional[float] = 1.0,
 ) -> Callable:
     """Create a function that will call the `generate` method of a `transformers` model.
 
@@ -38,29 +38,34 @@ def HuggingFaceCompletion(
     A function that will generate tokens from the model when passed a prompt.
 
     """
-    if max_tokens is None:
-        max_tokens = 216
 
-    if temperature is None:
-        temperature = 1.0
-
-    def call(prompt: str) -> str:
-        return call_model_generate_method(model_name, prompt, max_tokens, temperature)
+    def call(prompt: Union[str, List[str]], num_samples: int = 1) -> str:
+        return call_model_generate_method(
+            model_name, prompt, num_samples, max_tokens, temperature
+        )
 
     return call
 
 
 @memory.cache()
 def call_model_generate_method(
-    model_name: str, prompt: str, max_tokens: int, temperature: float
+    model_name: str,
+    prompt: Union[str, List[str]],
+    num_samples: int,
+    max_tokens: int,
+    temperature: float,
 ) -> str:
     import torch
     from transformers import AutoModelForCausalLM, AutoTokenizer
 
-    tokenizer = AutoTokenizer.from_pretrained(model_name)
+    tokenizer = AutoTokenizer.from_pretrained(model_name, padding_side="left")
     model = AutoModelForCausalLM.from_pretrained(model_name)
 
-    prompt_tokens = tokenizer(prompt, return_tensors="pt")
+    if isinstance(prompt, str):
+        prompt = [prompt]
+
+    tokenizer.pad_token = tokenizer.eos_token
+    prompt_tokens = tokenizer(prompt, return_tensors="pt", padding=True)
 
     if torch.cuda.is_available():
         model = model.to("cuda")
@@ -72,8 +77,35 @@ def call_model_generate_method(
         temperature=temperature,
         max_new_tokens=max_tokens,
         pad_token_id=tokenizer.eos_token_id,
+        num_return_sequences=num_samples,
     )
+
     new_tokens = returned_tokens[:, prompt_tokens["input_ids"].shape[1] + 1 :]
     new_tokens = new_tokens.squeeze()
+    new_sequences = decode(tokenizer, num_samples, new_tokens)
 
-    return tokenizer.decode(new_tokens, skip_special_tokens=True)
+    return new_sequences
+
+
+def decode(tokenizer, num_samples, new_tokens):
+    """Decode the output tokens.
+
+    The model's `generate` method will always return an array with at most
+    two dimensions, even with batched inputs and several samples. We thus
+    need to reshape the output list to return nested lists.
+
+    """
+    if new_tokens.ndim == 1:
+        return tokenizer.decode(new_tokens, skip_special_tokens=True)
+    else:
+        decoded = tokenizer.batch_decode(new_tokens, skip_special_tokens=True)
+
+        # Reshape the list if we have several samples
+        if num_samples > 1:
+            decoded = [
+                decoded[i : i + num_samples]
+                for i in range(0, len(decoded), num_samples)
+            ]
+            if len(decoded) == 1:
+                decoded = decoded[0]
+        return decoded
