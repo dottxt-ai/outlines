b505d7f0c50f6818389bed55fb5cd8f2d2060234
Rémi Louf <remi@thetypicalset.com>
2023-05-04 17:03:58 +0200

    Add Sphinx external linkcheck

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -14,7 +14,7 @@ release = "0.1"
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = ["sphinx_design"]
+extensions = ["sphinx.builders.linkcheck", "sphinx_design"]
 
 templates_path = ["_templates"]
 
