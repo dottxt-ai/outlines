74927fb5e0e6eadf927d1a54fcbdb640a09078af
Rémi Louf <remi@thetypicalset.com>
2023-05-10 22:54:51 +0200

    Take different samples from the `diffusers` library

--- outlines/models/hf_diffusers.py
+++ outlines/models/hf_diffusers.py
@@ -16,12 +16,16 @@ def HuggingFaceDiffuser(model_name: str) -> PILImage:
 
     """
 
-    def call(prompt: str) -> str:
-        return call_stable_diffusion_pipeline(model_name, prompt)
+    def call(prompt: str, samples: int = 1) -> str:
+        return call_stable_diffusion_pipeline(model_name, prompt, samples)
+
+    return call
 
 
 @memory.cache()
-def call_stable_diffusion_pipeline(model_name: str, prompt: str) -> PILImage:
+def call_stable_diffusion_pipeline(
+    model_name: str, prompt: str, samples: int
+) -> PILImage:
     """Build and call the Stable Diffusion pipeline."""
     import torch
     from diffusers import StableDiffusionPipeline
@@ -29,6 +33,6 @@ def call_stable_diffusion_pipeline(model_name: str, prompt: str) -> PILImage:
     pipe = StableDiffusionPipeline.from_pretrained(model_name)
     if torch.cuda.is_available():
         pipe = pipe.to("cuda")
-    image = pipe(prompt).images[0]
+    image = pipe(prompt, num_images_per_prompt=samples).images[0]
 
     return image
