d28498a44659043259cce80d4195bea9e9278856
Rémi Louf <remi@thetypicalset.com>
2023-05-10 22:56:33 +0200

    Add self-consistency example

---
+++ examples/self-consistency.py
@@ -0,0 +1,74 @@
+import re
+
+import numpy as np
+
+import outlines.models as models
+import outlines.text as text
+
+examples = [
+    {
+        "question": "There are 15 trees in the grove. Grove workers will plant trees in the grove today. After they are done, there will be 21 trees. How many trees did the grove workers plant today?",
+        "answer": "We start with 15 trees. Later we have 21 trees. The difference must be the number of trees they planted. So, they must have planted 21 - 15 = 6 trees. The answer is 6.",
+    },
+    {
+        "question": "If there are 3 cars in the parking lot and 2 more cars arrive, how many cars are in the parking lot?",
+        "answer": "There are 3 cars in the parking lot already. 2 more arrive. Now there are 3 + 2 = 5 cars. The answer is 5.",
+    },
+    {
+        "question": "Leah had 32 chocolates and her sister had 42. If they ate 35, how many pieces do they have left in total?",
+        "answer": "Leah had 32 chocolates and Leah’s sister had 42. That means there were originally 32 + 42 = 74 chocolates. 35 have been eaten. So in total they still have 74 - 35 = 39 chocolates. The answer is 39.",
+    },
+    {
+        "question": "Jason had 20 lollipops. He gave Denny some lollipops. Now Jason has 12 lollipops. How many lollipops did Jason give to Denny?",
+        "answer": "Jason had 20 lollipops. Since he only has 12 now, he must have given the rest to Denny. The number of lollipops he has given to Denny must have been 20 - 12 = 8 lollipops. The answer is 8.",
+    },
+    {
+        "question": "Shawn has five toys. For Christmas, he got two toys each from his mom and dad. How many toys does he have now?",
+        "answer": "He has 5 toys. He got 2 from mom, so after that he has 5 + 2 = 7 toys. Then he got 2 more from dad, so in total he has 7 + 2 = 9 toys. The answer is 9.",
+    },
+    {
+        "question": "There were nine computers in the server room. Five more computers were installed each day, from monday to thursday. How many computers are now in the server room?",
+        "answer": "There are 4 days from monday to thursday. 5 computers were added each day. That means in total 4 * 5 = 20 computers were added. There were 9 computers in the beginning, so now there are 9 + 20 = 29 computers. The answer is 29.",
+    },
+    {
+        "question": "Michael had 58 golf balls. On tuesday, he lost 23 golf balls. On wednesday, he lost 2 more. How many golf balls did he have at the end of wednesday?",
+        "answer": "Michael initially had 58 balls. He lost 23 on Tuesday, so after that he has 58 - 23 = 35 balls. On Wednesday he lost 2 more so now he has 35 - 2 = 33 balls. The answer is 33.",
+    },
+    {
+        "question": "Olivia has $23. She bought five bagels for $3 each. How much money does she have left?",
+        "answer": "She bought 5 bagels for $3 each. This means she spent 5",
+    },
+]
+
+question = "When I was 6 my sister was half my age. Now I’m 70 how old is my sister?"
+
+
+@text.prompt
+def few_shots(question, examples):
+    """
+    {% for example in examples %}
+    Q: {{ example.question }}
+    A: {{ example.answer }}
+    {% endfor %}
+    Q: {{ question }}
+    A:
+    """
+
+
+model = models.text_completion.openai("text-davinci-003", max_tokens=128)
+prompt = few_shots(question, examples)
+answers = model(prompt, samples=100)
+
+digits = []
+for answer in answers:
+    try:
+        match = re.findall(r"\d+", answer)[-1]
+        if match is not None:
+            digit = int(match)
+            digits.append(digit)
+    except AttributeError:
+        print(f"Could not parse the completion: '{answer}'")
+
+unique_digits, counts = np.unique(digits, return_counts=True)
+results = {d: c for d, c in zip(unique_digits, counts)}
+print(results)
