71791458fb9e8d0030da200c704894677e7fb3c1
Rémi Louf <remi@thetypicalset.com>
2023-05-10 16:31:54 +0200

    Add documentation to `outlines.text.function`

--- outlines/text/functions.py
+++ outlines/text/functions.py
@@ -10,6 +10,25 @@ BaseModelType = type(BaseModel)
 
 @dataclass
 class function:
+    """Represents a function that uses a language model to generate its output.
+
+    When called, the `function` instance passes the arguments to the prompt
+    function, the rendered prompt is passed to the language model, and its
+    result to an (optional) validation function.
+
+    Attributes
+    ----------
+    model
+        A function that takes a string and returns a string that contains the
+        model's return value.
+    prompt
+        A prompt-generating function.
+    validator
+        A function that takes the output of the language model, parses it and
+        returns it in a normalized format.
+
+    """
+
     model: Callable
     prompt: Callable
     validator: Optional[Union[Callable, BaseModel]] = None
