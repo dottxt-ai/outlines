aec7639f9d0cf3e8d28eda67c1309dc355f22435
Rémi Louf <remi@thetypicalset.com>
2023-05-15 14:10:00 +0200

    Add tests for diffusers integration

--- outlines/models/hf_diffusers.py
+++ outlines/models/hf_diffusers.py
@@ -1,8 +1,6 @@
 """Integration with HuggingFace's `diffusers` library."""
 from PIL.Image import Image as PILImage
 
-from outlines.caching import cache
-
 
 def HuggingFaceDiffuser(model_name: str) -> PILImage:
     """Create a function that will call a stable diffusion pipeline.
@@ -15,22 +13,28 @@ def HuggingFaceDiffuser(model_name: str) -> PILImage:
     """
 
     def call(prompt: str, samples: int = 1) -> str:
-        return call_stable_diffusion_pipeline(model_name, prompt, samples)
+        results = call_stable_diffusion_pipeline(model_name, prompt, samples)
+        if samples == 1:
+            return results[0]
+        return results
 
     return call
 
 
-@cache
 def call_stable_diffusion_pipeline(
     model_name: str, prompt: str, samples: int
 ) -> PILImage:
-    """Build and call the Stable Diffusion pipeline."""
+    """Build and call the Stable Diffusion pipeline.
+
+    We convert the returned image
+    """
     import torch
     from diffusers import StableDiffusionPipeline
 
     pipe = StableDiffusionPipeline.from_pretrained(model_name)
     if torch.cuda.is_available():
         pipe = pipe.to("cuda")
-    image = pipe(prompt, num_images_per_prompt=samples).images[0]
 
-    return image
+    images = pipe(prompt, num_images_per_prompt=samples).images
+
+    return images
