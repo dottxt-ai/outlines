5a40ac113edf41b86205187e35aad04fcbea52c8
Rémi Louf <remi@thetypicalset.com>
2023-05-15 14:10:00 +0200

    Add tests for diffusers integration

---
+++ tests/models/test_hf_diffusers.py
@@ -0,0 +1,18 @@
+from PIL.Image import Image as PILImage
+
+from outlines.models.hf_diffusers import HuggingFaceDiffuser
+
+MODEL = "hf-internal-testing/tiny-stable-diffusion-torch"
+
+
+def test_stable_diffusion():
+    model = HuggingFaceDiffuser(MODEL)
+
+    image = model("test")
+    assert isinstance(image, PILImage)
+
+    images = model("test", samples=3)
+    assert isinstance(images, list)
+    assert len(images) == 3
+    for img in images:
+        assert isinstance(image, PILImage)
