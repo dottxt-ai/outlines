580d1137c35014c40d9eeaae2b2aaae3922613bb
Brandon T. Willard <brandonwillard@users.noreply.github.com>
2023-07-06 15:20:49 -0500

    Extend the parsing example to include SQL-guided generation

--- examples/parsing.py
+++ examples/parsing.py
@@ -1,6 +1,7 @@
 """An example illustrating parser-based masking."""
 import math
 import time
+import urllib.request
 
 import torch
 from lark import Lark
@@ -26,23 +27,33 @@ model = AutoModelForCausalLM.from_pretrained(
     checkpoint, trust_remote_code=True, revision=revision
 ).to(device)
 
-input_text = "def "
-inputs = tokenizer.encode(input_text, return_tensors="pt").to(device)
+sql_grammar_url = "https://github.com/zbrookle/sql_to_ibis/raw/0e9226da42065940ce21439d490f9fcacadc7f92/sql_to_ibis/grammar/sql.lark"
+sql_grammar = "".join(
+    [line.decode("utf-8") for line in urllib.request.urlopen(sql_grammar_url)]
+)
+with open("sql_grammar.lark", "w") as f:
+    f.write(sql_grammar)
+
+sqlparser = Lark.open(
+    "sql_grammar.lark",
+    parser="lalr",
+)
+
+pyparser = Lark.open_from_package(
+    "lark",
+    "python.lark",
+    ["grammars"],
+    parser="lalr",
+    postlex=PartialPythonIndenter(),
+    start="file_input",
+)
 
 
 class ParserLogitsProcessor(LogitsProcessor):
     """Bias invalid token scores according to a running parse state."""
 
-    def __init__(self):
-        pyparser = Lark.open_from_package(
-            "lark",
-            "python.lark",
-            ["grammars"],
-            parser="lalr",
-            postlex=PartialPythonIndenter(),
-            start="file_input",
-        )
-        ip = pyparser.parse_interactive("")
+    def __init__(self, parser):
+        ip = parser.parse_interactive("")
         self.parser_state = ip.parser_state
         self.states_stack = [self.parser_state]
         self.token_seq = None
@@ -64,10 +75,8 @@ class ParserLogitsProcessor(LogitsProcessor):
 
         self.parser_state, partial_tokens = parse_to_end(self.parser_state)
 
-        print("Parsed:\n")
-        print(self.token_seq)
-
-        print(partial_tokens)
+        print(f'parsed:"{self.token_seq}"')
+        print(f"partial_tokens: {partial_tokens}")
 
         mask = torch.full_like(scores, -math.inf)
 
@@ -90,18 +99,22 @@ class ParserLogitsProcessor(LogitsProcessor):
             except (UnexpectedToken, UnexpectedCharacters, DedentError):
                 pass
 
-        print(f"Next token masking duration: {time.perf_counter() - t0}")
+        print(f"next token masking duration: {time.perf_counter() - t0}")
 
         return scores + mask
 
 
 set_seed(20399)
 
+parser = sqlparser
+input_text = "select "
+inputs = tokenizer.encode(input_text, return_tensors="pt").to(device)
+
 outputs = model.generate(
     inputs,
     max_length=100,
     temperature=0.1,
-    logits_processor=LogitsProcessorList([ParserLogitsProcessor()]),
+    logits_processor=LogitsProcessorList([ParserLogitsProcessor(parser)]),
     renormalize_logits=True,
 )
 
