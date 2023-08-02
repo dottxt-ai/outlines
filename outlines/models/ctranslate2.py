1d83f8f6b78dd4966ed38ff309f823777734d5f3
Arun Patro <arun.kumar.patro@gmail.com>
2023-08-01 21:29:51 -0400

    fix

--- outlines/models/ctranslate2.py
+++ outlines/models/ctranslate2.py
@@ -2,8 +2,9 @@ import math
 from typing import TYPE_CHECKING, List, Optional, Tuple, Union
 
 import torch
+import numpy as np
 
-from outlines.models.tokenizer import Tokenizer
+from outlines.models.transformers import TransformersTokenizer
 
 if TYPE_CHECKING:
     from transformers import PreTrainedTokenizer
@@ -30,57 +31,21 @@ class CTranslate2_Model:
         self, input_ids: torch.LongTensor, attention_mask: torch.LongTensor
     ) -> torch.FloatTensor:
         # `forward_batch` method of `Generator` accepts `tokens` in a list of list of str
+        
         tokens = [self.tokenizer.tokenizer.convert_ids_to_tokens(iids) for iids in input_ids]
         logits = self.model.forward_batch(tokens, return_log_probs=True)
-        logits = torch.as_tensor(logits) # this could be making the code slow
+        if self.device == 'cpu':
+            # See: https://github.com/OpenNMT/CTranslate2/issues/1386
+            logits = np.array(logits)
+        logits = torch.as_tensor(logits)
         next_token_logits = logits[:, -1, :]
 
         return next_token_logits
 
 
-class TransformersTokenizer(Tokenizer):
-    """Represents a tokenizer for models in the `transformers` library."""
-
-    def __init__(self, model_name: str, **kwargs):
-        from transformers import AutoTokenizer
-
-        kwargs.setdefault("padding_side", "left")
-        self.tokenizer = AutoTokenizer.from_pretrained(model_name, **kwargs)
-        self.eos_token_id = self.tokenizer.eos_token_id
-        self.eos_token = self.tokenizer.eos_token
-
-        if not self.tokenizer.pad_token_id:
-            self.tokenizer.pad_token_id = self.tokenizer.eos_token_id
-            self.pad_token_id = self.eos_token_id
-        else:
-            self.pad_token_id = self.tokenizer.pad_token_id
-            self.pad_token = self.tokenizer.pad_token
-
-        self.vocabulary = self.tokenizer.get_vocab()
-
-    def encode(
-        self, prompt: Union[str, List[str]], **kwargs
-    ) -> Tuple[torch.LongTensor, torch.LongTensor]:
-        kwargs["padding"] = True
-        kwargs["return_tensors"] = "pt"
-        output = self.tokenizer(prompt, **kwargs)
-
-        return output["input_ids"], output["attention_mask"]
-
-    def decode(self, token_ids: torch.LongTensor) -> List[str]:
-        text = self.tokenizer.batch_decode(token_ids)
-        return text
-
-    def convert_token_to_string(self, token: str) -> str:
-        string = self.tokenizer.convert_tokens_to_string([token])
-        return string
-
-
-def ctranslate2(
-    ctr2_model: str, tokenizer_name: str, device: Optional[str] = None, **model_kwargs
-):
+def ctranslate2(ctr2_model: str, tokenizer_name: str, device: Optional[str] = None, **model_kwargs):
     import ctranslate2
-
+    
     model = ctranslate2.Generator(ctr2_model, device=device)
     tokenizer = TransformersTokenizer(tokenizer_name, **model_kwargs)
 
