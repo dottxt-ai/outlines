ef90f557be62fad19337eeb72db2b6ec837ac386
John <51100181+jwmza@users.noreply.github.com>
2023-08-20 17:47:03 -0400

    Fix typo in models index

--- outlines/models/__init__.py
+++ outlines/models/__init__.py
@@ -1,4 +1,4 @@
-"""Module that contains all the models integated in outlines.
+"""Module that contains all the models integrated in outlines.
 
 We group the models in submodules by provider instead of theme (completion, chat
 completion, diffusers, etc.) and use routing functions everywhere else in the
