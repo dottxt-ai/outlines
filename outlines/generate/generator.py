121e983fb8f4e75f5404349dac501cb4f62d6d7d
Rémi Louf <remi@thetypicalset.com>
2023-11-15 12:15:37 +0100

    Add generator that samples the next tokens

--- outlines/generate/generator.py
+++ outlines/generate/generator.py
@@ -1,82 +1,140 @@
+import dataclasses
 import math
-from dataclasses import dataclass
-from typing import TYPE_CHECKING, Generator, List, Optional
+from typing import TYPE_CHECKING, Callable, List, Optional, Union
 
 import torch
 
+from outlines.index.index import FSMState
+
 if TYPE_CHECKING:
     from outlines.generate.samplers import Sampler
-    from outlines.index.index import Index
+    from outlines.index.index import FSM
 
 
-@dataclass
+@dataclasses.dataclass(frozen=True)
 class GenerationState:
     token_ids: torch.Tensor
     attention_masks: torch.Tensor
     kv_cache: Optional[torch.Tensor] = None
 
 
-def process(generator: Generator, index: "Index", state: GenerationState):
-    """This generator drives the text generation process by
-    walking through the FSM."""
-    next(generator)
+class SequenceGenerator:
+    def __init__(self, fsm, model, sampler, device):
+        self.generate_token = token_generator(model, sampler)
+        self.fsm = fsm
+        self.tokenizer = model.tokenizer
+        self.device = device
 
-    fsm_states = [0 for _ in range(state.token_ids.shape[0])]
+    def init_generation_state(
+        self,
+        prompt: Union[str, List[str]],
+        kv_cache: Optional[torch.Tensor] = None,
+        rng: Optional[torch.Generator] = None,
+    ):
+        """Initialize the generation state.
+
+        This method is responsible for encoding the prompt, moving token ids
+        to the device and initializing the random number generator.
+
+        Parameters
+        ----------
+        prompt
+            The prompt on which the generation is conditioned.
+        rng
+            The state of the random number generator.
+
+        Returns
+        -------
+        A `GenerationState` object.
+
+        """
+        token_ids, attention_masks = self.tokenizer.encode(prompt)
+        token_ids = token_ids.to(self.device)
+        attention_masks = attention_masks.to(self.device)
+
+        return GenerationState(token_ids, attention_masks, kv_cache)
+
+    def __call__(
+        self,
+        prompt,
+        kv_cache: Optional[torch.tensor] = None,
+        rng: Optional[torch.Generator] = None,
+    ):
+        sequence_generator = self.stream(prompt, rng)
+        *_, last = sequence_generator
+        return last
+
+    def stream(
+        self,
+        prompt: str,
+        kv_cache: Optional[torch.tensor] = None,
+        rng: Optional[torch.Generator] = None,
+    ):
+        if rng is None:
+            rng = torch.Generator(device=self.device)
+            rng.seed()
+
+        init_state = self.init_generation_state(prompt, kv_cache, rng)
+
+        num_sequences = init_state.token_ids.shape[0]
+        init_fsm_states = [FSMState(0) for _ in range(num_sequences)]
+
+        return sequence_generator(
+            self.generate_token, self.fsm, init_state, init_fsm_states, rng
+        )
+
+
+def sequence_generator(
+    token_generator: Callable,
+    fsm: "FSM",
+    init_state: GenerationState,
+    fsm_states: List[FSMState],
+    rng: torch.Generator,
+):
+    """Generates sequences of tokens.
+
+    Parameters
+    ----------
+    token_generator
+        A callable that generate a new token given the current generation state
+        and logits biases.
+    fsm
+        The finite-state machine that drives the text generation.
+    init_state
+        The initial generation state for the batches.
+    fsm_states
+        The initial states of the finite-state machine for each sequence in the batch.
+
+    Yields
+    ------
+    A new generation state.
+
+    """
+    state = init_state
     while True:
-        logits_mask = get_next_instructions(index, fsm_states)
+        logits_masks = get_next_instructions(fsm, fsm_states)
 
-        next_token_ids, kv_cache = generator.send((state, logits_mask))
+        next_token_ids, kv_cache = token_generator(
+            **dataclasses.asdict(state),
+            rng=rng,
+            logits_masks=logits_masks,
+        )
 
         token_ids = update_token_ids(state.token_ids, next_token_ids)
-        attention_masks = update_attention_masks(state.attention_masks)
+        attention_masks = expand_attention_masks(state.attention_masks)
         state = GenerationState(token_ids, attention_masks, kv_cache)
 
-        fsm_states = get_next_fsm_states(index, fsm_states, next_token_ids)
-        is_finished = is_generation_finished(index, fsm_states)
+        fsm_states = get_next_fsm_states(fsm, fsm_states, next_token_ids)
+        is_finished = is_generation_finished(fsm, fsm_states)
         if is_finished:
-            yield token_ids, next_token_ids
+            yield state
             return
 
         yield state
 
 
-def get_next_fsm_states(
-    index, fsm_states: List[int], next_token_ids: torch.Tensor
-) -> List[int]:
-    return [
-        index.next_state(fsm_state, token_id)
-        for fsm_state, token_id in zip(fsm_states, next_token_ids)
-    ]
-
-
-def get_next_instructions(index, fsm_states: List[int]) -> torch.Tensor:
-    return [index.next_instruction(state) for state in fsm_states]
-
-
-def is_generation_finished(index, fsm_states: List[int]) -> bool:
-    return all([index.is_finished(state) for state in fsm_states])
-
-
-def update_token_ids(
-    token_ids: torch.Tensor, next_token_ids: torch.Tensor
-) -> torch.Tensor:
-    return torch.concatenate([token_ids, next_token_ids], dim=1 - 1)
-
-
-def update_attention_masks(attention_masks: torch.Tensor) -> torch.Tensor:
-    return torch.concatenate(
-        [
-            attention_masks,
-            torch.ones(
-                attention_masks.shape[:-1] + (1,), device=attention_masks.device
-            ),
-        ],
-        axis=-1,
-    )
-
-
-def token_generator(model, sampler: "Sampler", samples: int, rng: torch.Generator):
-    """Generator that yields a token every time it is called.
+def token_generator(model, sampler: "Sampler") -> Callable:
+    """Generate one token at a time.
 
     This process is designed to be steered by another supervising
     process that supplies the current sequence and the indices
@@ -91,25 +149,146 @@ def token_generator(model, sampler: "Sampler", samples: int, rng: torch.Generato
         A function that samples tokens from a probability
         distribution over the next tokens.
 
-    Yields
-    ------
+    Returns
+    -------
     A tensor with the sampled tokens.
 
     """
-    while True:
-        (token_ids, attention_masks, kv_cache), logits_mask = yield
 
+    def generate(
+        token_ids,
+        attention_masks,
+        kv_cache,
+        logits_masks,
+        rng: torch.Generator,
+    ) -> Union[torch.Tensor, torch.Tensor, torch.Tensor, torch.Tensor]:
         try:
             logits, new_kv_cache = model(token_ids, attention_masks, kv_cache)
         except IndexError:  # Exceeding the context length
-            return
+            raise IndexError(
+                "The input length exceeds the context length of the model."
+            )
 
-        biased_logits = bias_logits(logits, logits_mask)
-        next_token_ids = sampler(biased_logits, samples, rng)
+        biased_logits = bias_logits(logits, logits_masks)
+        next_token_ids = sampler(biased_logits, 1, rng)
 
-        yield next_token_ids, new_kv_cache
+        return next_token_ids, new_kv_cache
+
+    return generate
 
 
+def get_next_fsm_states(
+    fsm: "FSM", fsm_states: List[FSMState], next_token_ids: torch.Tensor
+) -> List[FSMState]:
+    """
+
+    Parameters
+    ----------
+    fsm
+        The finite-state machine used to monitor this batch.
+    next_token_ids
+        The tokens that were just generated.
+
+    Returns
+    -------
+    A `torch.Tensor` object that represents the next logit mask.
+
+    """
+    return [
+        fsm.next_state(fsm_state, token_id)
+        for fsm_state, token_id in zip(fsm_states, next_token_ids)
+    ]
+
+
+def get_next_instructions(fsm: "FSM", fsm_states: List[FSMState]) -> torch.Tensor:
+    """Get the new instructions for each sequence from the finite-state machine.
+
+    Parameters
+    ----------
+    fsm
+        The finite-state machine used to monitor this batch.
+    fsm_states
+        The FSM states corresponding to each sequence in the batch.
+
+    Returns
+    -------
+    A nested list that contains the ids of the logits to bias.
+
+    """
+    return [fsm.next_instruction(state) for state in fsm_states]
+
+
+def is_generation_finished(fsm: "FSM", fsm_states: List[FSMState]) -> bool:
+    """Determine if the generation is finished.
+
+    A generation is considered finished if the FSM of every sequence in the
+    batch is in a final state.
+
+    A better solution is to return finished sequences as soon as their FSM
+    is in a final state.
+
+    Parameters
+    ----------
+    fsm
+        The finite-state machine used to monitor this batch.
+    fsm_states
+        The FSM states corresponding to each sequence in the batch.
+
+    Returns
+    -------
+    Whether all sequences are finished sampling.
+
+    """
+    return all([fsm.is_final_state(state) for state in fsm_states])
+
+
+def update_token_ids(
+    token_ids: torch.Tensor, next_token_ids: torch.Tensor
+) -> torch.Tensor:
+    """Append the sampled tokens to the running sequence of tokens.
+
+    Parameters
+    ----------
+    token_ids
+        The current token sequences
+    next_token_ids
+        The tokens that were just generated and that we need to append
+        to the existing sequences.
+
+    Returns
+    -------
+    A new sequence of token ids that contains the tokens that were
+    just generated.
+
+    """
+    return torch.concatenate([token_ids, next_token_ids], dim=-1)
+
+
+def expand_attention_masks(attention_masks: torch.Tensor) -> torch.Tensor:
+    """Expand the attention masks.
+
+    Parameters
+    ----------
+    attention_masks
+        The attention masks for each sequence in the batch.
+
+    Returns
+    -------
+    The attention masks padded with 1s.
+
+    """
+    return torch.concatenate(
+        [
+            attention_masks,
+            torch.ones(
+                attention_masks.shape[:-1] + (1,), device=attention_masks.device
+            ),
+        ],
+        axis=-1,
+    )
+
+
+@torch.inference_mode()
 def bias_logits(
     logits: torch.Tensor,
     ids_to_mask: List,
--- tests/generate/test_generator.py
+++ tests/generate/test_generator.py
@@ -1,9 +1,278 @@
 import math
+from typing import Generator
 
 import pytest
 import torch
 
-from outlines.generate.generator import bias_logits, token_generator
+from outlines.generate.generator import (
+    GenerationState,
+    SequenceGenerator,
+    bias_logits,
+    expand_attention_masks,
+    get_next_fsm_states,
+    get_next_instructions,
+    is_generation_finished,
+    sequence_generator,
+    token_generator,
+    update_token_ids,
+)
+from outlines.index.index import FSMState
+
+
+def test_sequence_generator_class():
+    class MockFSM:
+        def next_state(self, state, next_token_ids):
+            return 0
+
+        def next_instruction(self, _):
+            return []
+
+        def is_final_state(self, _):
+            return True
+
+    class MockTokenizer:
+        def encode(self, _):
+            return torch.tensor([[0, 1, 2, 3]]), torch.tensor([[1, 1, 1, 1]])
+
+    class MockModel:
+        def __init__(self):
+            self.tokenizer = MockTokenizer()
+
+        def __call__(*_):
+            return torch.tensor([[0, 1, 2, 3]], dtype=torch.float), None
+
+    def sampler(biased_logits, *_):
+        return torch.argmax(biased_logits, keepdims=True)
+
+    # Stream
+    generator = SequenceGenerator(MockFSM(), MockModel(), sampler, "cpu")
+    assert generator.device == "cpu"
+    assert isinstance(generator.tokenizer, MockTokenizer)
+    assert isinstance(generator.fsm, MockFSM)
+    assert callable(generator.generate_token)
+
+    result = generator.init_generation_state("test")
+    assert torch.equal(result.token_ids, torch.tensor([[0, 1, 2, 3]]))
+    assert torch.equal(result.attention_masks, torch.tensor([[1, 1, 1, 1]]))
+    assert result.kv_cache is None
+
+    sequence = generator.stream("test")
+    assert isinstance(sequence, Generator)
+
+    state = next(sequence)
+    assert torch.equal(state.token_ids, torch.tensor([[0, 1, 2, 3, 3]]))
+    assert torch.equal(state.attention_masks, torch.tensor([[1, 1, 1, 1, 1]]))
+
+    with pytest.raises(StopIteration):
+        state = next(sequence)
+
+    # Call
+    generator = SequenceGenerator(MockFSM(), MockModel(), sampler, "cpu")
+    result = generator("test")
+    assert torch.equal(state.token_ids, torch.tensor([[0, 1, 2, 3, 3]]))
+    assert torch.equal(state.attention_masks, torch.tensor([[1, 1, 1, 1, 1]]))
+
+
+def test_sequence_generator_1d_single_iteration():
+    class MockFSM:
+        def next_state(self, state, next_token_ids):
+            return 0
+
+        def next_instruction(self, _):
+            return []
+
+        def is_final_state(self, _):
+            return True
+
+    class MockTokenizer:
+        def encode(self, _):
+            return torch.tensor([[0, 1, 2, 3]]), torch.tensor([[1, 1, 1, 1]])
+
+    class MockModel:
+        def __init__(self):
+            self.tokenizer = MockTokenizer()
+
+        def __call__(*_):
+            return torch.tensor([[0, 1, 2, 3]], dtype=torch.float), None
+
+    def sampler(biased_logits, *_):
+        return torch.argmax(biased_logits, keepdims=True)
+
+    init_state = GenerationState(
+        torch.tensor([[0, 1, 2, 3]]), torch.tensor([[1, 1, 1, 1]]), None
+    )
+    init_fsm_states = [0]
+    generate = token_generator(MockModel(), sampler)
+    sequence = sequence_generator(
+        generate, MockFSM(), init_state, init_fsm_states, torch.Generator()
+    )
+    result = next(sequence)
+
+    assert torch.equal(result.token_ids, torch.tensor([[0, 1, 2, 3, 3]]))
+    assert torch.equal(result.attention_masks, torch.tensor([[1, 1, 1, 1, 1]]))
+
+    with pytest.raises(StopIteration):
+        next(sequence)
+
+
+def test_sequence_generator_1d_several_iterations():
+    class MockFSM:
+        def next_state(self, state, next_token_ids):
+            return FSMState(state + 1)
+
+        def next_instruction(self, _):
+            return []
+
+        def is_final_state(self, state):
+            if state < 2:
+                return False
+            else:
+                return True
+
+    class MockTokenizer:
+        def encode(self, _):
+            return torch.tensor([[0, 1, 2, 3]]), torch.tensor([[1, 1, 1, 1]])
+
+    class MockModel:
+        def __init__(self):
+            self.tokenizer = MockTokenizer()
+
+        def __call__(*_):
+            return torch.tensor([[0, 1, 2, 3]], dtype=torch.float), None
+
+    def sampler(biased_logits, *_):
+        return torch.argmax(biased_logits, keepdims=True)
+
+    init_state = GenerationState(
+        torch.tensor([[0, 1, 2, 3]]), torch.tensor([[1, 1, 1, 1]]), None
+    )
+    init_fsm_states = [0]
+    generate = token_generator(MockModel(), sampler)
+    sequence = sequence_generator(
+        generate, MockFSM(), init_state, init_fsm_states, torch.Generator()
+    )
+
+    result = next(sequence)
+    assert torch.equal(result.token_ids, torch.tensor([[0, 1, 2, 3, 3]]))
+    assert torch.equal(result.attention_masks, torch.tensor([[1, 1, 1, 1, 1]]))
+
+    result = next(sequence)
+    assert torch.equal(result.token_ids, torch.tensor([[0, 1, 2, 3, 3, 3]]))
+    assert torch.equal(result.attention_masks, torch.tensor([[1, 1, 1, 1, 1, 1]]))
+
+    with pytest.raises(StopIteration):
+        next(sequence)
+
+
+def test_sequence_generator_2d_single_iteration():
+    class MockFSM:
+        def next_state(self, state, next_token_ids):
+            return 0
+
+        def next_instruction(self, _):
+            return []
+
+        def is_final_state(self, _):
+            return True
+
+    class MockTokenizer:
+        def encode(self, _):
+            return torch.tensor([[0, 1, 2, 3], [4, 5, 6, 7]]), torch.tensor(
+                [[1, 1, 1, 1], [1, 1, 1, 1]]
+            )
+
+    class MockModel:
+        def __init__(self):
+            self.tokenizer = MockTokenizer()
+
+        def __call__(*_):
+            return torch.tensor([[0, 1, 2, 3], [4, 5, 7, 6]], dtype=torch.float), None
+
+    def sampler(biased_logits, *_):
+        return torch.argmax(biased_logits, keepdims=True, dim=-1)
+
+    init_state = GenerationState(
+        torch.tensor([[0, 1, 2, 3], [4, 5, 6, 7]]),
+        torch.tensor([[1, 1, 1, 1], [1, 1, 1, 1]]),
+        None,
+    )
+    init_fsm_states = [0, 0]
+    generate = token_generator(MockModel(), sampler)
+    sequence = sequence_generator(
+        generate, MockFSM(), init_state, init_fsm_states, torch.Generator()
+    )
+
+    result = next(sequence)
+    assert torch.equal(
+        result.token_ids, torch.tensor([[0, 1, 2, 3, 3], [4, 5, 6, 7, 2]])
+    )
+    assert torch.equal(
+        result.attention_masks, torch.tensor([[1, 1, 1, 1, 1], [1, 1, 1, 1, 1]])
+    )
+
+    with pytest.raises(StopIteration):
+        next(sequence)
+
+
+def test_sequence_generator_2d_several_iterations():
+    class MockFSM:
+        def next_state(self, state, next_token_ids):
+            return FSMState(state + 1)
+
+        def next_instruction(self, _):
+            return []
+
+        def is_final_state(self, state):
+            if state < 2:
+                return False
+            else:
+                return True
+
+    class MockTokenizer:
+        def encode(self, _):
+            return torch.tensor([[0, 1, 2, 3], [4, 5, 6, 7]]), torch.tensor(
+                [[1, 1, 1, 1], [1, 1, 1, 1]]
+            )
+
+    class MockModel:
+        def __init__(self):
+            self.tokenizer = MockTokenizer()
+
+        def __call__(*_):
+            return torch.tensor([[0, 1, 2, 3], [4, 5, 7, 6]], dtype=torch.float), None
+
+    def sampler(biased_logits, *_):
+        return torch.argmax(biased_logits, keepdims=True, dim=-1)
+
+    init_state = GenerationState(
+        torch.tensor([[0, 1, 2, 3], [4, 5, 6, 7]]),
+        torch.tensor([[1, 1, 1, 1], [1, 1, 1, 1]]),
+        None,
+    )
+    init_fsm_states = [0, 0]
+    generate = token_generator(MockModel(), sampler)
+    sequence = sequence_generator(
+        generate, MockFSM(), init_state, init_fsm_states, torch.Generator()
+    )
+
+    result = next(sequence)
+    assert torch.equal(
+        result.token_ids, torch.tensor([[0, 1, 2, 3, 3], [4, 5, 6, 7, 2]])
+    )
+    assert torch.equal(
+        result.attention_masks, torch.tensor([[1, 1, 1, 1, 1], [1, 1, 1, 1, 1]])
+    )
+
+    result = next(sequence)
+    assert torch.equal(
+        result.token_ids, torch.tensor([[0, 1, 2, 3, 3, 3], [4, 5, 6, 7, 2, 2]])
+    )
+    assert torch.equal(
+        result.attention_masks, torch.tensor([[1, 1, 1, 1, 1, 1], [1, 1, 1, 1, 1, 1]])
+    )
+
+    with pytest.raises(StopIteration):
+        next(sequence)
 
 
 def test_generator_error():
@@ -13,10 +282,127 @@ def test_generator_error():
     def sampler():
         return None
 
-    generator = token_generator(model, sampler, 1, None)
-    next(generator)
-    with pytest.raises(StopIteration):
-        generator.send(((None, None, None), None))
+    generator = token_generator(model, sampler)
+    with pytest.raises(IndexError, match="The input length"):
+        generator(None, None, None, None, None)
+
+
+@pytest.mark.parametrize(
+    "logits_biases,expected_result",
+    [
+        ([[]], [[3]]),
+        ([[3]], [[2]]),
+        ([[2, 3]], [[1]]),
+    ],
+)
+def test_generator_1d(logits_biases, expected_result):
+    def model(*_):
+        return torch.tensor([[0, 1, 2, 3]], dtype=torch.float), None
+
+    def sampler(biased_logits, *_):
+        return torch.argmax(biased_logits, keepdims=True)
+
+    generator = token_generator(model, sampler)
+    result, _ = generator(None, None, None, logits_biases, None)
+    assert torch.equal(result, torch.tensor(expected_result))
+
+
+@pytest.mark.parametrize(
+    "logits_biases,expected_result",
+    [
+        ([[]], [[3], [3]]),
+        ([[3], [3]], [[2], [2]]),
+        ([[3], []], [[2], [3]]),
+        ([[2, 3], [3]], [[1], [2]]),
+    ],
+)
+def test_generator_2d(logits_biases, expected_result):
+    def model(*_):
+        return torch.tensor([[0, 1, 2, 3], [4, 5, 6, 7]], dtype=torch.float), None
+
+    def sampler(biased_logits, *_):
+        return torch.argmax(biased_logits, dim=1, keepdims=True)
+
+    generator = token_generator(model, sampler)
+    result, _ = generator(None, None, None, logits_biases, None)
+    assert torch.equal(result, torch.tensor(expected_result))
+
+
+def test_get_next_fsm_states():
+    class MockFSM:
+        def next_state(self, state, next_token_ids):
+            return 0
+
+    result = get_next_fsm_states(MockFSM(), [0], torch.tensor([[0]]))
+    assert result == [0]
+
+    result = get_next_fsm_states(MockFSM(), [0, 0], torch.tensor([[0], [0]]))
+    assert result == [0, 0]
+
+
+def test_get_next_instructions():
+    class MockFSM:
+        def next_instruction(self, _):
+            return [1, 2, 3, 4]
+
+    result = get_next_instructions(MockFSM(), [0])
+    assert result == [[1, 2, 3, 4]]
+
+    result = get_next_instructions(MockFSM(), [0, 1])
+    assert result == [[1, 2, 3, 4], [1, 2, 3, 4]]
+
+
+def test_is_generation_finished():
+    class MockFSMFinished:
+        def is_final_state(self, _):
+            return True
+
+    result = is_generation_finished(MockFSMFinished(), [1, 1])
+    assert result is True
+
+    class MockFSMNotFinished:
+        def is_final_state(self, state):
+            if state == 0:
+                return False
+            else:
+                return True
+
+    result = is_generation_finished(MockFSMNotFinished(), [0, 1])
+    assert result is False
+
+
+@pytest.mark.parametrize(
+    "token_ids,next_token_ids,expected_result",
+    [
+        (torch.tensor([[1]]), torch.tensor([[2]]), torch.tensor([[1, 2]])),
+        (
+            torch.tensor([[1], [1]]),
+            torch.tensor([[2], [3]]),
+            torch.tensor([[1, 2], [1, 3]]),
+        ),
+    ],
+)
+def test_update_token_ids(token_ids, next_token_ids, expected_result):
+    result = update_token_ids(token_ids, next_token_ids)
+    assert torch.equal(result, expected_result)
+
+
+@pytest.mark.parametrize(
+    "attention_masks,expected_result",
+    [
+        (
+            torch.tensor([[1, 1]], dtype=torch.float),
+            torch.tensor([[1, 1, 1]], dtype=torch.float),
+        ),
+        (
+            torch.tensor([[1, 1], [1, 1]], dtype=torch.float),
+            torch.tensor([[1, 1, 1], [1, 1, 1]], dtype=torch.float),
+        ),
+    ],
+)
+def test_expand_attention_masks(attention_masks, expected_result):
+    result = expand_attention_masks(attention_masks)
+    assert torch.equal(result, expected_result)
 
 
 @pytest.mark.parametrize(
@@ -54,86 +440,3 @@ def test_generator_error():
 def test_bias_logits(logits, indices_to_mask, expected):
     masked_logits = bias_logits(logits, indices_to_mask)
     assert torch.equal(masked_logits, expected)
-
-
-def test_generator_1d():
-    def model(*_):
-        return torch.tensor([[0, 1, 2, 3]], dtype=torch.float), None
-
-    def sampler(biased_logits, *_):
-        return torch.argmax(biased_logits)
-
-    # 1D, no bias
-    generator = token_generator(model, sampler, 1, None)
-    next(generator)
-    result, _ = generator.send(((None, None, None), [[]]))
-    assert result == 3
-
-    # 1D, bias one
-    generator = token_generator(model, sampler, 1, None)
-    next(generator)
-    result, _ = generator.send(((None, None, None), [[3]]))
-    assert result == 2
-
-    # 1D, bias two
-    generator = token_generator(model, sampler, 1, None)
-    next(generator)
-    result, _ = generator.send(((None, None, None), [[2, 3]]))
-    assert result == 1
-
-
-def test_generator_2d():
-    def model(*_):
-        return torch.tensor([[0, 1, 2, 3], [4, 5, 6, 7]], dtype=torch.float), None
-
-    def sampler(biased_logits, *_):
-        return torch.argmax(biased_logits, dim=1)
-
-    # 2D, no bias
-    generator = token_generator(model, sampler, 1, None)
-    next(generator)
-    result, _ = generator.send(((None, None, None), [[]]))
-    assert torch.equal(result, torch.tensor([3, 3]))
-
-    # 2D, bias one each
-    generator = token_generator(model, sampler, 1, None)
-    next(generator)
-    result, _ = generator.send(((None, None, None), [[3], [3]]))
-    assert torch.equal(result, torch.tensor([2, 2]))
-
-    # 2D, bias one
-    generator = token_generator(model, sampler, 1, None)
-    next(generator)
-    result, _ = generator.send(((None, None, None), [[3], []]))
-    assert torch.equal(result, torch.tensor([2, 3]))
-
-    # 2D, bias different number
-    generator = token_generator(model, sampler, 1, None)
-    next(generator)
-    result, _ = generator.send(((None, None, None), [[3], [2, 3]]))
-    assert torch.equal(result, torch.tensor([2, 1]))
-
-
-@pytest.mark.xfail
-def get_next_fsm_states():
-    raise NotImplementedError
-
-
-@pytest.mark.xfail
-def get_next_instructions():
-    raise NotImplementedError
-
-
-@pytest.mark.xfail
-def is_generation_finished():
-    raise NotImplementedError
-
-
-@pytest.mark.xfail
-def update_token_ids():
-    raise NotImplementedError
-
-
-@pytest.mark.xfail
-def update_attention_masks():
-    raise NotImplementedError
