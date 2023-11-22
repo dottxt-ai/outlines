9c77ce175f859cc1dcc3aa5442256a504b2deca6
Rémi Louf <remilouf@gmail.com>
2023-11-22 15:39:39 +0100

    Create Regex FSM

--- outlines/index/index.py
+++ outlines/index/index.py
@@ -1,18 +1,18 @@
-from dataclasses import dataclass
-from typing import List, NewType, Optional, Protocol
+from typing import TYPE_CHECKING, List, NewType, Optional, Protocol
 
+import interegular
 import torch
+
+from outlines.index.fsm import create_fsm_index_tokenizer, make_deterministic_fsm
+
+if TYPE_CHECKING:
+    from outlines.models.tokenizer import Tokenizer
 
 FSMState = NewType("FSMState", int)
 
 
-@dataclass(frozen=True)
-class GenerateInstruction:
-    tokens_to_mask: List[int]
-
-
 class FSM(Protocol):
-    def next_instruction(self, state: FSMState) -> GenerateInstruction:
+    def next_instruction(self, state: FSMState) -> torch.Tensor:
         ...
 
     def next_state(self, state: FSMState, token_id: torch.Tensor) -> FSMState:
@@ -28,8 +28,8 @@ class StopAtTokenFSM:
         self.max_tokens = max_tokens
         self.num_tokens_generated = 0
 
-    def next_instructions(self, _: FSMState) -> GenerateInstruction:
-        return GenerateInstruction([])
+    def next_instruction(self, _: FSMState) -> List[int]:
+        return []
 
     def next_state(self, state: FSMState, token_id: torch.Tensor) -> FSMState:
         self.num_tokens_generated += 1
@@ -50,3 +50,75 @@ class StopAtTokenFSM:
             return True
         else:
             return False
+
+
+class RegexFSM:
+    def __init__(
+        self,
+        regex_string: str,
+        tokenizer: "Tokenizer",
+        max_tokens: Optional[int] = None,
+    ):
+        regex_pattern = interegular.parse_pattern(regex_string)
+        regex_fsm, _ = make_deterministic_fsm(regex_pattern.to_fsm().reduce())
+        (
+            self.states_to_token_maps,
+            self.empty_token_ids,
+        ) = create_fsm_index_tokenizer(regex_fsm, tokenizer)
+
+        if not any(
+            regex_fsm.finals.intersection(v.values())
+            for v in self.states_to_token_maps.values()
+        ):
+            raise ValueError(
+                "The vocabulary does not allow us to build a sequence that matches the input regex"
+            )
+
+        self.final_states = regex_fsm.finals | {
+            -1
+        }  # Include the EOS token in final states
+        self.max_tokens = max_tokens
+        self.num_tokens_generated = 0
+        self.vocabulary = tokenizer.vocabulary.values()
+        self.end_token = tokenizer.eos_token_id
+
+    def next_instruction(self, state: FSMState) -> List[int]:
+        next_tokens_to_end_states = self.states_to_token_maps.get(state)
+
+        if next_tokens_to_end_states is None:
+            # If there are no transitions from the current state,
+            # then we must've been in a final state of the FSM.
+            # We produce EOS tokens from here on.
+            authorized_tokens = [self.end_token]
+        else:
+            authorized_tokens = list(next_tokens_to_end_states.keys())
+
+        forbidden_tokens = [
+            token for token in self.vocabulary if token not in authorized_tokens
+        ]
+
+        return list(forbidden_tokens)
+
+    def next_state(self, state: FSMState, token_id: torch.Tensor) -> FSMState:
+        self.num_tokens_generated += 1
+
+        if token_id == self.end_token:
+            return FSMState(-1)
+
+        last_token_to_end_state = self.states_to_token_maps[state]
+        next_state = last_token_to_end_state.get(int(token_id))
+        if next_state is None:
+            next_state = -1
+
+        return FSMState(next_state)
+
+    def is_final_state(self, state: FSMState) -> bool:
+        # Stop if the maximum number of tokens has been generated
+        # regardless of whether the stop token id has been found.
+        if self.max_tokens is not None:
+            if self.num_tokens_generated == self.max_tokens:
+                return True
+        elif state in self.final_states:
+            return True
+
+        return False
---
+++ tests/index/test_index.py
@@ -0,0 +1,50 @@
+import pytest
+
+from outlines.index.index import RegexFSM, StopAtTokenFSM
+
+
+def test_stop_at_token():
+    fsm = StopAtTokenFSM(1)
+
+    assert fsm.next_instruction(0) == []
+    assert fsm.next_state(0, 10) == 0
+    assert fsm.next_state(0, 1) == 1
+    assert fsm.is_final_state(0) is False
+    assert fsm.is_final_state(1) is True
+
+
+def test_regex_vocabulary_error():
+    class MockTokenizer:
+        vocabulary = {"a": 1}
+        special_tokens = {"eos"}
+
+        def convert_token_to_string(self, token):
+            return token
+
+    regex_str = "[1-9]"
+
+    with pytest.raises(ValueError, match="The vocabulary"):
+        RegexFSM(regex_str, MockTokenizer())
+
+
+def test_regex():
+    class MockTokenizer:
+        vocabulary = {"1": 1, "a": 2, "eos": 3}
+        special_tokens = {"eos"}
+        eos_token_id = 3
+
+        def convert_token_to_string(self, token):
+            return token
+
+    regex_str = "[1-9]"
+    tokenizer = MockTokenizer()
+    fsm = RegexFSM(regex_str, tokenizer)
+
+    assert fsm.states_to_token_maps == {0: {1: 1}}
+    assert fsm.next_instruction(state=0) == [2, 3]
+    assert fsm.next_state(state=0, token_id=1) == 1
+    assert fsm.next_state(state=0, token_id=tokenizer.eos_token_id) == -1
+
+    assert fsm.is_final_state(1) is True
+    assert fsm.is_final_state(0) is False
+    assert fsm.is_final_state(-1) is True
