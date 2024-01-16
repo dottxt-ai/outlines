07da67b5da17e58276fb9b7eb41c1e8cde5385b1
Andrew Lapp <andrew@rew.la>
2024-01-16 12:17:24 -0600

    update caching order

--- tests/benchmark/test_benchmark_numba_compile.py
+++ tests/benchmark/test_benchmark_numba_compile.py
@@ -9,8 +9,6 @@ outlines.disable_cache()
 
 from outlines.fsm import regex  # noqa: E402
 
-outlines.disable_cache()
-
 
 def test_benchmark_compile_numba(benchmark, tokenizer, mocker):
     """Compile a basic regex to benchmark the numba compilation time"""
