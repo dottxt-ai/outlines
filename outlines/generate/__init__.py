159ff44bd7516ed43f8fd21bf78589c7f7fe73ae
Rémi Louf <remilouf@gmail.com>
2024-01-24 11:56:47 +0100

    Dispatch `json` based on the model passed by the user

--- outlines/generate/__init__.py
+++ outlines/generate/__init__.py
@@ -1,4 +1,5 @@
-from .api import SequenceGenerator, cfg, json, regex
+from .api import SequenceGenerator, cfg, regex
 from .choice import choice
 from .format import format
+from .json import json
 from .text import text
