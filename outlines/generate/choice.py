99525abf49d11176feb6d85708898f808c49c3cd
Rémi Louf <remilouf@gmail.com>
2024-01-24 11:47:05 +0100

    Dispatch `choice` based on the model passed by the user

---
+++ outlines/generate/choice.py
@@ -0,0 +1,37 @@
+from functools import singledispatch
+from typing import Callable, List, Optional
+
+from outlines.generate.samplers import Sampler, multinomial
+from outlines.models import OpenAI
+
+from .api import regex
+
+
+@singledispatch
+def choice(
+    model,
+    choices: List[str],
+    max_tokens: Optional[int] = None,
+    sampler: Sampler = multinomial,
+):
+    regex_str = r"(" + r"|".join(choices) + r")"
+    return regex(model, regex_str, max_tokens, sampler)
+
+
+@choice.register(OpenAI)
+def choice_openai(
+    model: OpenAI,
+    choices: List[str],
+    max_tokens: Optional[int] = None,
+    sampler: Sampler = multinomial,
+) -> Callable:
+    if not sampler == multinomial:
+        raise NotImplementedError(
+            r"The OpenAI API does not support any other sampling algorithm "
+            + "that the multinomial sampler."
+        )
+
+    def generate_choice(prompt: str, max_tokens: int = 1):
+        return model.generate_choice(prompt, choices, max_tokens)
+
+    return generate_choice
