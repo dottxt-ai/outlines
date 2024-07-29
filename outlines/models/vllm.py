0c00137aca096aeeb7e862d606144f8ce06cec3d
Franz Louis Cesista <franzlouiscesista@gmail.com>
2024-07-30 04:31:55 +0800

    rm extra tokenizer

--- outlines/models/vllm.py
+++ outlines/models/vllm.py
@@ -4,8 +4,6 @@ from typing import TYPE_CHECKING, List, Optional, Union
 from outlines.generate.api import GenerationParameters, SamplingParameters
 from outlines.integrations.utils import adapt_tokenizer
 
-from .transformers import TransformerTokenizer
-
 if TYPE_CHECKING:
     from vllm import LLM
     from vllm.sampling_params import SamplingParams
@@ -23,7 +21,6 @@ class VLLM:
 
     def __init__(self, model: "LLM"):
         self.model = model
-        self.tokenizer = TransformerTokenizer(self.model.get_tokenizer())
         self.lora_request = None
 
         self.tokenizer = self._get_tokenizer()
