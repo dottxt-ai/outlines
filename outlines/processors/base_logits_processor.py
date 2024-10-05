d1b8728c6c65355f53a2c78432f8b07ad7518b7a
Andrew Lapp <andrew@github.rew.la>
2024-10-05 16:29:21 -0400

    construct logits mask in batch operation

--- outlines/processors/base_logits_processor.py
+++ outlines/processors/base_logits_processor.py
@@ -75,10 +75,10 @@ class OutlinesLogitsProcessor(Protocol):
 
         # Guarantee passed as 2D Tensors, then covert back to original (1D or 2D) shape
         if len(torch_logits.shape) == 2:
-            processed_logits = self.process_logits(input_ids.tolist(), torch_logits)
+            processed_logits = self.process_logits(input_ids, torch_logits)
         elif len(torch_logits.shape) == 1:
             processed_logits = self.process_logits(
-                [input_ids.tolist()], torch_logits.unsqueeze(0)
+                input_ids.unsqueeze(0), torch_logits.unsqueeze(0)
             ).squeeze(0)
 
         # return logits as passed array type
