4ef8d9653003502f20c5f288c5761b74a1e97e90
Aakash Thatte <aathatte2002@gmail.com>
2024-10-14 19:00:59 +0530

    optimize tests

--- tests/processors/test_base_processor.py
+++ tests/processors/test_base_processor.py
@@ -1,7 +1,6 @@
 from typing import List
 
 import jax.numpy as jnp
-import jaxlib
 import numpy as np
 import pytest
 import torch
@@ -14,11 +13,15 @@ try:
 except ImportError:
     MLX_AVAILABLE = False
 
-from outlines.processors.base_logits_processor import (
-    OutlinesLogitsProcessor,
-    is_jax_array_type,
-    is_mlx_array_type,
-)
+from outlines.processors.base_logits_processor import OutlinesLogitsProcessor
+
+arrays = {
+    "list": [[1.0, 2.0], [3.0, 4.0]],
+    "np": np.array([[1, 2], [3, 4]], dtype=np.float32),
+    "jax": jnp.array([[1, 2], [3, 4]], dtype=jnp.float32),
+    "torch": torch.tensor([[1, 2], [3, 4]], dtype=torch.float32),
+    "mlx": mx.array([[1, 2], [3, 4]], dtype=mx.float32),
+}
 
 
 # Mock implementation of the abstract class for testing
@@ -36,111 +39,31 @@ def processor():
     return MockLogitsProcessor()
 
 
-# ---- Test _to_torch() ---- #
-def test_to_torch_with_numpy(processor):
-    np_array = np.array([[1, 2], [3, 4]], dtype=np.float32)
-    torch_tensor = processor._to_torch(np_array)
-    assert isinstance(torch_tensor, torch.Tensor)
-    assert torch.allclose(
-        torch_tensor, torch.tensor([[1, 2], [3, 4]], dtype=torch.float32)
-    )
-
-
-def test_to_torch_with_list(processor):
-    data_list = [[1, 2], [3, 4]]
-    torch_tensor = processor._to_torch(data_list)
-    assert isinstance(torch_tensor, torch.Tensor)
-    assert torch.allclose(
-        torch_tensor, torch.tensor([[1, 2], [3, 4]], dtype=torch.int64)
-    )
-
-
-def test_to_torch_with_jax(processor):
-    jax_array = jnp.array([[1, 2], [3, 4]], dtype=jnp.float32)
-    torch_tensor = processor._to_torch(jax_array)
+@pytest.mark.parametrize("array_type", arrays.keys())
+def test_to_torch(array_type, processor):
+    data = arrays[array_type]
+    torch_tensor = processor._to_torch(data)
     assert isinstance(torch_tensor, torch.Tensor)
     assert torch.allclose(
         torch_tensor.cpu(), torch.tensor([[1, 2], [3, 4]], dtype=torch.float32)
     )
 
 
-@pytest.mark.skipif(not MLX_AVAILABLE, reason="mlx is not installed")
-def test_to_torch_with_mlx(processor):
-    mlx_array = mx.array([[1, 2], [3, 4]], dtype=np.float32)
-    torch_tensor = processor._to_torch(mlx_array)
-    assert isinstance(torch_tensor, torch.Tensor)
-    assert torch.allclose(
-        torch_tensor, torch.tensor([[1, 2], [3, 4]], dtype=torch.float32)
-    )
-
-
-# ---- Test _from_torch() ---- #
-def test_from_torch_to_numpy(processor):
+@pytest.mark.parametrize("array_type", arrays.keys())
+def test_from_torch(array_type, processor):
     torch_tensor = torch.tensor([[1, 2], [3, 4]], dtype=torch.float32)
-    np_array = processor._from_torch(torch_tensor, np.ndarray)
-    assert isinstance(np_array, np.ndarray)
-    assert np.allclose(np_array, np.array([[1, 2], [3, 4]], dtype=np.float32))
+    data = processor._from_torch(torch_tensor, type(arrays[array_type]))
+    assert isinstance(data, type(arrays[array_type]))
+    assert np.allclose(data, arrays[array_type])
 
 
-def test_from_torch_to_list(processor):
-    torch_tensor = torch.tensor([[1, 2], [3, 4]], dtype=torch.float32)
-    data_list = processor._from_torch(torch_tensor, list)
-    assert isinstance(data_list, list)
-    assert data_list == [[1, 2], [3, 4]]
-
-
-def test_from_torch_to_jax(processor):
-    torch_tensor = torch.tensor([[1, 2], [3, 4]], dtype=torch.float32)
-    jax_array = processor._from_torch(torch_tensor, jaxlib.xla_extension.ArrayImpl)
-    print(f"JAX TYPE: {is_jax_array_type(type(jax_array))}")
-    assert is_jax_array_type(type(jax_array))
-    assert np.allclose(
-        np.array(jax_array), np.array([[1, 2], [3, 4]], dtype=np.float32)
-    )
-
-
-@pytest.mark.skipif(not MLX_AVAILABLE, reason="mlx is not installed")
-def test_from_torch_to_mlx(processor):
-    torch_tensor = torch.tensor([[1, 2], [3, 4]], dtype=torch.float32)
-    mlx_array = processor._from_torch(torch_tensor, mx.array)
-    assert is_mlx_array_type(type(mlx_array))
-    assert np.allclose(
-        np.array(mlx_array), np.array([[1, 2], [3, 4]], dtype=np.float32)
-    )
-
-
-# ---- Test process_logits ---- #
-def test_process_logits(processor):
-    input_ids = [[1, 2], [3, 4]]
-    logits = torch.tensor([[1.0, 2.0], [3.0, 4.0]], dtype=torch.float32)
-    processed_logits = processor.process_logits(input_ids, logits)
-
-    # multiplied by 2 as per mock implementation
-    assert torch.allclose(
-        processed_logits, torch.tensor([[2.0, 4.0], [6.0, 8.0]], dtype=torch.float32)
-    )
-
-
-# ---- Test __call__() ---- #
-def test_call_with_numpy(processor):
-    input_ids = np.array([[1, 2], [3, 4]], dtype=np.int32)
-    logits = np.array([[0.5, 0.2], [0.1, 0.9]], dtype=np.float32)
+@pytest.mark.parametrize("array_type", arrays.keys())
+def test_call(array_type, processor):
+    input_ids = arrays[array_type]
+    logits = arrays[array_type]
     processed_logits = processor(input_ids, logits)
 
-    # utput should match the mock behavior (logits * 2) and be in the original format (numpy)
-    assert isinstance(processed_logits, np.ndarray)
+    assert isinstance(processed_logits, type(arrays[array_type]))
     assert np.allclose(
-        processed_logits, np.array([[1.0, 0.4], [0.2, 1.8]], dtype=np.float32)
-    )
-
-
-def test_call_with_jax(processor):
-    input_ids = jnp.array([[1, 2], [3, 4]], dtype=jnp.int32)
-    logits = jnp.array([[0.5, 0.2], [0.1, 0.9]], dtype=jnp.float32)
-    processed_logits = processor(input_ids, logits)
-
-    # output should match the mock behavior (logits * 2) and be in the original format (JAX array)
-    assert is_jax_array_type(type(processed_logits))
-    assert np.allclose(
-        np.array(processed_logits), np.array([[1.0, 0.4], [0.2, 1.8]], dtype=np.float32)
+        np.array(processed_logits), np.array([[2.0, 4.0], [6.0, 8.0]], dtype=np.float32)
     )
