6e76096b1025054c0146f7a18a9bd282adc78a3f
Rémi Louf <remi@thetypicalset.com>
2024-11-05 09:48:09 +0100

    Refactor the `OpenAI` interface

--- tests/models/test_openai.py
+++ tests/models/test_openai.py
@@ -1,125 +1,107 @@
-import importlib
+import io
 import json
-from contextlib import contextmanager
-from unittest import mock
-from unittest.mock import MagicMock
 
+import PIL
 import pytest
-from openai import AsyncOpenAI
+import requests
+from pydantic import BaseModel
 
-from outlines import generate
-from outlines.models.openai import OpenAI, OpenAIConfig
+from outlines.models.openai import OpenAI
+from outlines.prompts import Vision
+from outlines.types import Json
 
-
-def module_patch(path):
-    """Patch functions that have the same name as the module in which they're implemented."""
-    target = path
-    components = target.split(".")
-    for i in range(len(components), 0, -1):
-        try:
-            # attempt to import the module
-            imported = importlib.import_module(".".join(components[:i]))
-
-            # module was imported, let's use it in the patch
-            patch = mock.patch(path)
-            patch.getter = lambda: imported
-            patch.attribute = ".".join(components[i:])
-            return patch
-        except Exception:
-            continue
-
-    # did not find a module, just return the default mock
-    return mock.patch(path)
+MODEL_NAME = "gpt-4o-mini-2024-07-18"
 
 
-def test_openai_call():
-    with module_patch("outlines.models.openai.generate_chat") as mocked_generate_chat:
-        mocked_generate_chat.return_value = ["foo"], 1, 2
-        async_client = MagicMock(spec=AsyncOpenAI, api_key="key")
-
-        model = OpenAI(
-            async_client,
-            OpenAIConfig(max_tokens=10, temperature=0.5, n=2, stop=["."]),
-        )
-
-        assert model("bar")[0] == "foo"
-        assert model.prompt_tokens == 1
-        assert model.completion_tokens == 2
-        mocked_generate_chat_args = mocked_generate_chat.call_args
-        mocked_generate_chat_arg_config = mocked_generate_chat_args[0][3]
-        assert isinstance(mocked_generate_chat_arg_config, OpenAIConfig)
-        assert mocked_generate_chat_arg_config.max_tokens == 10
-        assert mocked_generate_chat_arg_config.temperature == 0.5
-        assert mocked_generate_chat_arg_config.n == 2
-        assert mocked_generate_chat_arg_config.stop == ["."]
-
-        model("bar", samples=3)
-        mocked_generate_chat_args = mocked_generate_chat.call_args
-        mocked_generate_chat_arg_config = mocked_generate_chat_args[0][3]
-        assert mocked_generate_chat_arg_config.n == 3
+def test_openai_wrong_init_parameters():
+    with pytest.raises(TypeError, match="got an unexpected"):
+        OpenAI(MODEL_NAME, foo=10)
 
 
-@contextmanager
-def patched_openai(completion, **oai_config):
-    """Create a patched openai whose chat completions always returns `completion`"""
-    with module_patch("outlines.models.openai.generate_chat") as mocked_generate_chat:
-        mocked_generate_chat.return_value = completion, 1, 2
-        async_client = MagicMock(spec=AsyncOpenAI, api_key="key")
-        model = OpenAI(
-            async_client,
-            OpenAIConfig(max_tokens=10, temperature=0.5, n=2, stop=["."]),
-        )
-        yield model
+def test_openai_wrong_inference_parameters():
+    with pytest.raises(TypeError, match="got an unexpected"):
+        model = OpenAI(MODEL_NAME)
+        model.generate("prompt", foo=10)
 
 
-def test_openai_choice_call():
-    with patched_openai(completion='{"result": "foo"}') as model:
-        generator = generate.choice(model, ["foo", "bar"])
-        assert generator("hi") == "foo"
+def test_openai_wrong_input_type():
+    class Foo:
+        def __init__(self, foo):
+            self.foo = foo
+
+    with pytest.raises(NotImplementedError, match="is not available"):
+        model = OpenAI(MODEL_NAME)
+        model.generate(Foo("prompt"))
 
 
-def test_openai_choice_call_invalid_server_response():
-    with patched_openai(completion="not actual json") as model:
-        generator = generate.choice(model, ["foo", "bar"])
-        with pytest.raises(json.decoder.JSONDecodeError):
-            generator("hi")
+def test_openai_wrong_output_type():
+    class Foo:
+        def __init__(self, foo):
+            self.foo = foo
+
+    with pytest.raises(NotImplementedError, match="is not available"):
+        model = OpenAI(MODEL_NAME)
+        model.generate("prompt", Foo(1))
 
 
-def test_openai_json_call_pydantic():
-    from pydantic import BaseModel, ConfigDict, ValidationError
-
-    class Person(BaseModel):
-        model_config = ConfigDict(extra="forbid")  # required for openai
-        first_name: str
-        last_name: str
-        age: int
-
-    completion = '{"first_name": "Usain", "last_name": "Bolt", "age": 38}'
-
-    # assert success for valid response
-    with patched_openai(completion=completion) as model:
-        generator = generate.json(model, Person)
-        assert generator("fastest person") == Person.parse_raw(completion)
-
-    # assert fail for non-json response
-    with patched_openai(completion="usain bolt") as model:
-        generator = generate.json(model, Person)
-        with pytest.raises(ValidationError):
-            assert generator("fastest person")
+@pytest.mark.api_call
+def test_openai_simple_call():
+    model = OpenAI(MODEL_NAME)
+    result = model.generate("Respond with one word. Not more.")
+    assert isinstance(result, str)
 
 
-def test_openai_json_call_str():
-    person_schema = '{"additionalProperties": false, "properties": {"first_name": {"title": "First Name", "type": "string"}, "last_name": {"title": "Last Name", "type": "string"}, "age": {"title": "Age", "type": "integer"}}, "required": ["first_name", "last_name", "age"], "title": "Person", "type": "object"}'
+@pytest.mark.api_call
+def test_openai_simple_vision():
+    model = OpenAI(MODEL_NAME)
 
-    output = {"first_name": "Usain", "last_name": "Bolt", "age": 38}
+    url = "https://raw.githubusercontent.com/dottxt-ai/outlines/refs/heads/main/docs/assets/images/logo.png"
+    r = requests.get(url, stream=True)
+    if r.status_code == 200:
+        image = PIL.Image.open(io.BytesIO(r.content))
 
-    # assert success for valid response
-    with patched_openai(completion=json.dumps(output)) as model:
-        generator = generate.json(model, person_schema)
-        assert generator("fastest person") == output
+    result = model.generate(Vision("What does this logo represent?", image))
+    assert isinstance(result, str)
 
-    # assert fail for non-json response
-    with patched_openai(completion="usain bolt") as model:
-        generator = generate.json(model, person_schema)
-        with pytest.raises(json.decoder.JSONDecodeError):
-            assert generator("fastest person")
+
+@pytest.mark.api_call
+def test_openai_simple_pydantic():
+    model = OpenAI(MODEL_NAME)
+
+    class Foo(BaseModel):
+        bar: int
+
+    result = model.generate("foo?", Json(Foo))
+    assert isinstance(result, str)
+    assert "bar" in json.loads(result)
+
+
+@pytest.mark.api_call
+def test_openai_simple_vision_pydantic():
+    model = OpenAI(MODEL_NAME)
+
+    url = "https://raw.githubusercontent.com/dottxt-ai/outlines/refs/heads/main/docs/assets/images/logo.png"
+    r = requests.get(url, stream=True)
+    if r.status_code == 200:
+        image = PIL.Image.open(io.BytesIO(r.content))
+
+    class Logo(BaseModel):
+        name: int
+
+    result = model.generate(Vision("What does this logo represent?", image), Json(Logo))
+    assert isinstance(result, str)
+    assert "name" in json.loads(result)
+
+
+@pytest.mark.api_call
+def test_openai_simple_json_schema():
+    model = OpenAI(MODEL_NAME)
+
+    class Foo(BaseModel):
+        bar: int
+
+    schema = json.dumps(Foo.model_json_schema())
+
+    result = model.generate("foo?", Json(schema))
+    assert isinstance(result, str)
+    json.loads(result)
