af030c12e39641eab097929a8defdc978acc5e75
Rémi Louf <remilouf@protonmail.com>
2024-11-29 18:46:02 +0100

    Add `to_regex` method to the different types

--- outlines/types/__init__.py
+++ outlines/types/__init__.py
@@ -8,6 +8,8 @@ from jsonschema.exceptions import SchemaError
 from pydantic import BaseModel, TypeAdapter
 from typing_extensions import _TypedDictMeta  # type: ignore
 
+from outlines.fsm.json_schema import build_regex_from_schema
+
 from . import airports, countries
 from .email import Email
 from .isbn import ISBN
@@ -30,6 +32,7 @@ class Json:
     """
 
     definition: Union[str, dict]
+    whitespace_pattern: str = " "
 
     def to_json_schema(self):
         if isinstance(self.definition, str):
@@ -52,10 +55,20 @@ class Json:
 
         return schema
 
+    def to_regex(self):
+        schema = self.to_json_schema()
+        schema_str = json.dumps(schema)
+        return build_regex_from_schema(schema_str, self.whitespace_pattern)
+
 
 @dataclass
 class List:
     definition: list
+
+    def to_regex(self):
+        raise NotImplementedError(
+            "Structured generation for lists of objects are not implemented yet."
+        )
 
 
 @dataclass
@@ -67,3 +80,24 @@ class Choice:
     def __post_init__(self):
         if isinstance(self.definition, list):
             self.definition = Enum("Definition", [(x, x) for x in self.definition])
+
+    def to_list(self):
+        if isinstance(self.definition, list):
+            return self.definition
+        else:
+            return [x.value for x in self.definition]
+
+    def to_regex(self):
+        choices = self.to_list()
+        regex_str = r"(" + r"|".join(choices) + r")"
+        return regex_str
+
+
+@dataclass
+class Regex:
+    """Represents a string defined by a regular expression."""
+
+    definition: str
+
+    def to_regex(self):
+        return self.definition
