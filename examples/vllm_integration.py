9beb9226cc1d6b177208cd33d1adb1e6e88ad64b
Marco De Nadai <me@marcodena.it>
2024-12-31 11:19:37 +0100

    Update vllm_integration.py

--- examples/vllm_integration.py
+++ examples/vllm_integration.py
@@ -16,7 +16,9 @@ class Person(BaseModel):
 MODEL_ID = "mistralai/Mistral-7B-v0.1"
 llm = vllm.LLM(model=MODEL_ID, max_model_len=512)
 tokenizer = adapt_tokenizer(AutoTokenizer.from_pretrained(MODEL_ID))
-logits_processor = JSONLogitsProcessor(schema=Person, tokenizer=tokenizer, whitespace_pattern=r" ?")
+logits_processor = JSONLogitsProcessor(
+    schema=Person, tokenizer=tokenizer, whitespace_pattern=r" ?"
+)
 result = llm.generate(
     ["He is Tom Jones", "She saw Linda Smith"],
     sampling_params=vllm.SamplingParams(
