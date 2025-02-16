b56acd1a7580b80a3ba4231ae64e482a1746b5ac
Robin Picard <robinpicard@device-36.home>
2025-02-16 21:24:27 +0100

    Create the Ollama model

--- outlines/models/__init__.py
+++ outlines/models/__init__.py
@@ -14,12 +14,15 @@ from .exllamav2 import ExLlamaV2Model, exl2
 from .gemini import Gemini
 from .llamacpp import LlamaCpp
 from .mlxlm import MLXLM, mlxlm
+from .ollama import Ollama
 from .openai import AzureOpenAI, OpenAI
 from .transformers import Transformers, TransformerTokenizer, mamba, transformers
 from .transformers_vision import TransformersVision, transformers_vision
 from .vllm import VLLM, vllm
 
-LogitsGenerator = Union[Transformers, LlamaCpp, OpenAI, ExLlamaV2Model, MLXLM, VLLM]
+LogitsGenerator = Union[
+    Transformers, LlamaCpp, OpenAI, ExLlamaV2Model, MLXLM, VLLM, Ollama
+]
 
 LocalModel = LlamaCpp
-APIModel = Union[AzureOpenAI, OpenAI, Anthropic, Gemini]
+APIModel = Union[AzureOpenAI, OpenAI, Anthropic, Gemini, Ollama]
