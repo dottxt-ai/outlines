bced7fe0b74ce8d1bca4b6ffe055f8580a5f0430
Rémi Louf <remilouf@protonmail.com>
2025-02-21 19:07:47 +0100

    Initialize `VLLM` from model instance

--- outlines/__init__.py
+++ outlines/__init__.py
@@ -10,20 +10,32 @@ from outlines.caching import clear_cache, disable_cache, get_cache
 from outlines.function import Function
 from outlines.templates import Template, prompt
 
-from outlines.models import from_openai, from_gemini, from_anthropic, from_ollama, from_llamacpp
+from outlines.models import (
+    from_openai,
+    from_gemini,
+    from_anthropic,
+    from_ollama,
+    from_llamacpp,
+    from_vllm,
+)
+
+
+models = [
+    "from_anthropic",
+    "from_gemini",
+    "from_llamacpp",
+    "from_ollama",
+    "from_openai",
+    "from_vllm",
+]
 
 __all__ = [
     "clear_cache",
     "disable_cache",
     "get_cache",
     "Function",
-    "from_anthropic",
-    "from_gemini",
-    "from_llamacpp",
-    "from_ollama",
-    "from_openai",
     "prompt",
     "Template",
     "vectorize",
     "grammars",
-]
+] + models
