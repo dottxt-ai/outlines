8a4c5af4302cf9907e4ef72139c2c09b7d3dca58
Robin Picard <83579270+RobinPicard@users.noreply.github.com>
2025-02-21 22:36:52 +0100

    Refactor the Transformers and TransformersVision model (#1430)
    The objective of this commit is to adapt `Transfomers` and `TransformersVision` to the
    model interface used in v1.0. The main changes are:
    - make the models inherit from the base class `Model` and create a
    `ModelTypeAdapter` for them
    - modify the signature of the `generate` method and make it the only way of
    generating text with the model.
    - modify the handling of model/tokenizer optional arguments to avoid manipulating
    them ourselves (giving to the user the responsibility of choosing those they need).
    - delete the tests for those models in the `generate` directory and test them instead
    in the `models` directory

--- outlines/models/transformers.py
+++ outlines/models/transformers.py
@@ -1,17 +1,14 @@
-import dataclasses
-import inspect
-from typing import TYPE_CHECKING, Iterator, List, Optional, Tuple, Union
+from functools import singledispatchmethod
+from typing import TYPE_CHECKING, List, Tuple, Union
 
-from outlines.generate.api import GenerationParameters, SamplingParameters
+from outlines.models.base import Model, ModelTypeAdapter
 from outlines.models.tokenizer import Tokenizer
 
 if TYPE_CHECKING:
     import torch
-    from transformers import PreTrainedModel, PreTrainedTokenizer
+    from transformers import PreTrainedTokenizer
 
-    from outlines.processors import OutlinesLogitsProcessor
-
-__all__ = ["transformers"]
+__all__ = ["Transformers", "Mamba"]
 
 
 KVCacheType = Tuple[Tuple["torch.DoubleTensor", "torch.DoubleTensor"], ...]
@@ -126,230 +123,130 @@ class TransformerTokenizer(Tokenizer):
         self.__init__(state["tokenizer"])
 
 
-class Transformers:
+class TransformersTypeAdapter(ModelTypeAdapter):
+    @singledispatchmethod
+    def format_input(self, model_input):
+        """Generate the prompt argument to pass to the model.
+
+        Argument
+        --------
+        model_input
+            The input passed by the user.
+
+        """
+        raise NotImplementedError(
+            f"The input type {input} is not available."
+            "Please use a string or a list of strings."
+        )
+
+    @format_input.register(str)
+    def format_str_input(self, model_input):
+        return model_input
+
+    @format_input.register(list)
+    def format_list_input(self, model_input):
+        return model_input
+
+    def format_output_type(self, output_type):
+        """Generate the logits processor argument to pass to the model.
+
+        Argument
+        --------
+        output_type
+            The logits processor provided.
+
+        """
+        from transformers import LogitsProcessorList
+
+        if output_type is not None:
+            return LogitsProcessorList([output_type])
+        return None
+
+
+class Transformers(Model):
     """Represents a `transformers` model."""
 
     def __init__(
         self,
-        model: "PreTrainedModel",
-        tokenizer: "PreTrainedTokenizer",
+        model_name: str,
+        model_class=None,
+        model_kwargs: dict = {},
+        tokenizer_class=None,
+        tokenizer_kwargs: dict = {},
     ):
-        self.model = model
-        self.tokenizer = TransformerTokenizer(tokenizer)
+        """Create a Transformers model instance
 
-    def forward(
-        self,
-        input_ids: "torch.LongTensor",
-        attention_mask: "torch.LongTensor",
-        past_key_values: Optional[Tuple] = None,
-    ) -> Tuple["torch.FloatTensor", Optional[KVCacheType]]:
-        """Compute a forward pass through the transformer model.
-
-        Parameters
+        Parameters:
         ----------
-        input_ids
-            The input token ids.  Must be one or two dimensional.
-        attention_mask
-            The attention mask.  Must be one or two dimensional.
-        past_key_values
-            A tuple of tuples containing the cached key and value tensors for each
-            attention head.
-
-        Returns
-        -------
-        The computed logits and the new cached key and value tensors.
+        model_name
+            The name of the transformers model to use;
+        model_class
+            The Transformers model class from which to create the model.
+            If not provided,`AutoModelForCausalLM` will be used.
+            If you gave the name of a non-causal language model,
+            you must provide a value for this parameter.
+        model_kwargs
+            A dictionary of keyword arguments to pass to the `from_pretrained`
+            method of the model class.
+        tokenizer_class
+            The Transformers tokenizer class from which to create the tokenizer.
+            If not provided,`AutoTokenizer` will be used.
+            If you gave the name of a model that is not compatible with `AutoTokenizer`,
+            you must provide a value for this parameter.
+        tokenizer_kwargs
+            A dictionary of keyword arguments to pass to the `from_pretrained`
+            method of the tokenizer class.
 
         """
-        try:
-            import torch
-        except ImportError:
-            ImportError(
-                "The `torch` library needs to be installed to use `transformers` models."
-            )
-        assert 0 < input_ids.ndim < 3
+        if model_class is None or tokenizer_class is None:
+            try:
+                from transformers import AutoModelForCausalLM, AutoTokenizer
+            except ImportError:
+                raise ImportError(
+                    "The `transformers` library needs to be installed in order to use `transformers` models."
+                )
+        if model_class is None:
+            model_class = AutoModelForCausalLM
+        if tokenizer_class is None:
+            tokenizer_class = AutoTokenizer
+        self.model = model_class.from_pretrained(model_name, **model_kwargs)
+        tokenizer_kwargs.setdefault("padding_side", "left")
+        self.tokenizer = TransformerTokenizer(
+            tokenizer_class.from_pretrained(model_name, **tokenizer_kwargs)
+        )
+        self.type_adapter = TransformersTypeAdapter()
 
-        if past_key_values:
-            input_ids = input_ids[..., -1].unsqueeze(-1)
-
-        with torch.inference_mode():
-            output = self.model(
-                input_ids,
-                attention_mask=attention_mask,
-                return_dict=True,
-                output_attentions=False,
-                output_hidden_states=False,
-                past_key_values=past_key_values,
-            )
-
-        return output.logits, output.past_key_values
-
-    def __call__(
-        self,
-        input_ids: "torch.LongTensor",
-        attention_mask: "torch.LongTensor",
-        past_key_values: Optional[Tuple] = None,
-    ) -> "torch.FloatTensor":
-        logits, kv_cache = self.forward(input_ids, attention_mask, past_key_values)
-        next_token_logits = logits[..., -1, :]
-
-        return next_token_logits, kv_cache
-
-    def generate(
-        self,
-        prompts: Union[str, List[str]],
-        generation_parameters: GenerationParameters,
-        logits_processor: Optional["OutlinesLogitsProcessor"],
-        sampling_parameters: SamplingParameters,
-    ) -> Union[str, List[str], List[List[str]]]:
-        """Generate text using `transformers`.
-
-        Parameters
-        ----------
-        prompts
-            A prompt or list of prompts.
-        generation_parameters
-            An instance of `GenerationParameters` that contains the prompt,
-            the maximum number of tokens, stop sequences and seed. All the
-            arguments to `SequenceGeneratorAdapter`'s `__cal__` method.
-        logits_processor
-            The logits processor to use when generating text.
-        sampling_parameters
-            An instance of `SamplingParameters`, a dataclass that contains
-            the name of the sampler to use and related parameters as available
-            in Outlines.
-
-        Returns
-        -------
-        The generated text
-        """
-        if isinstance(prompts, str):
-            # convert to 2d
-            input_ids, attention_mask = self.tokenizer.encode([prompts])
-        else:
-            input_ids, attention_mask = self.tokenizer.encode(prompts)
-
+    def generate(self, model_input, output_type, **inference_kwargs):
+        prompts = self.type_adapter.format_input(model_input)
+        input_ids, attention_mask = self.tokenizer.encode(prompts)
         inputs = {
             "input_ids": input_ids.to(self.model.device),
             "attention_mask": attention_mask.to(self.model.device),
         }
-        if (
-            "attention_mask"
-            not in inspect.signature(self.model.forward).parameters.keys()
-        ):
-            del inputs["attention_mask"]
 
-        generation_kwargs = self._get_generation_kwargs(
-            prompts,
-            generation_parameters,
-            logits_processor,
-            sampling_parameters,
+        logits_processor = self.type_adapter.format_output_type(output_type)
+
+        generated_ids = self._generate_output_seq(
+            prompts, inputs, logits_processor=logits_processor, **inference_kwargs
         )
-        generated_ids = self._generate_output_seq(prompts, inputs, **generation_kwargs)
 
-        # if single str input and single sample per input, convert to a 1D output
-        if isinstance(prompts, str):
+        # if single str input, convert to a 1D outputt
+        if isinstance(model_input, str):
             generated_ids = generated_ids.squeeze(0)
 
         return self._decode_generation(generated_ids)
 
-    def stream(
-        self,
-        prompts: Union[str, List[str]],
-        generation_parameters: GenerationParameters,
-        logits_processor: Optional["OutlinesLogitsProcessor"],
-        sampling_parameters: SamplingParameters,
-    ) -> Iterator[Union[str, List[str]]]:
+    def stream(self, model_input, output_type, **inference_kwargs):
         """
-        Temporary stream stand-in which implements stream() signature
-        and equivalent behaviour but isn't yielded until generation completes.
-
         TODO: implement following completion of https://github.com/huggingface/transformers/issues/30810
         """
-        if isinstance(prompts, str):
-            # convert to 2d
-            input_ids, attention_mask = self.tokenizer.encode([prompts])
-        else:
-            input_ids, attention_mask = self.tokenizer.encode(prompts)
-        inputs = {
-            "input_ids": input_ids.to(self.model.device),
-            "attention_mask": attention_mask.to(self.model.device),
-        }
-        if (
-            "attention_mask"
-            not in inspect.signature(self.model.forward).parameters.keys()
-        ):
-            del inputs["attention_mask"]
-
-        generation_kwargs = self._get_generation_kwargs(
-            prompts,
-            generation_parameters,
-            logits_processor,
-            sampling_parameters,
-        )
-        generated_ids = self._generate_output_seq(prompts, inputs, **generation_kwargs)
-
-        # if single str input and single sample per input, convert to a 1D output
-        if isinstance(prompts, str):
-            generated_ids = generated_ids.squeeze(0)
-
-        for i in range(generated_ids.size(-1)):
-            output_group_ids = generated_ids.select(-1, i).unsqueeze(-1)
-            yield self._decode_generation(output_group_ids)
-
-    def _get_generation_kwargs(
-        self,
-        prompts: Union[str, List[str]],
-        generation_parameters: GenerationParameters,
-        logits_processor: Optional["OutlinesLogitsProcessor"],
-        sampling_parameters: SamplingParameters,
-    ) -> dict:
-        """
-        Convert outlines generation parameters into model.generate kwargs
-        """
-        from transformers import GenerationConfig, LogitsProcessorList, set_seed
-
-        max_new_tokens, stop_at, seed = dataclasses.astuple(generation_parameters)
-        sampler, num_samples, top_p, top_k, temperature = dataclasses.astuple(
-            sampling_parameters
-        )
-        if max_new_tokens is None:
-            max_new_tokens = int(2**30)
-
-        # global seed, not desirable
-        if seed is not None:
-            set_seed(seed)
-
-        if logits_processor is not None:
-            logits_processor_list = LogitsProcessorList([logits_processor])
-        else:
-            logits_processor_list = None
-
-        generation_config = GenerationConfig(
-            max_new_tokens=max_new_tokens,
-            stop_strings=stop_at,
-            num_return_sequences=(num_samples or 1),
-            top_p=top_p,
-            top_k=top_k,
-            temperature=temperature,
-            do_sample=(sampler == "multinomial"),
-            num_beams=(num_samples if sampler == "beam_search" else 1),
-            eos_token_id=self.tokenizer.eos_token_id,
-            pad_token_id=self.tokenizer.pad_token_id,
+        raise NotImplementedError(
+            "Streaming is not implemented for Transformers models."
         )
 
-        return dict(
-            logits_processor=logits_processor_list,
-            generation_config=generation_config,
-            tokenizer=self.tokenizer.tokenizer,
-        )
-
-    def _generate_output_seq(
-        self, prompts, inputs, generation_config, **generation_kwargs
-    ):
+    def _generate_output_seq(self, prompts, inputs, **inference_kwargs):
         input_ids = inputs["input_ids"]
-        output_ids = self.model.generate(
-            **inputs, generation_config=generation_config, **generation_kwargs
-        )
+        output_ids = self.model.generate(**inputs, **inference_kwargs)
 
         # encoder-decoder returns output_ids only, decoder-only returns full seq ids
         if self.model.config.is_encoder_decoder:
@@ -358,12 +255,10 @@ class Transformers:
             generated_ids = output_ids[:, input_ids.shape[1] :]
 
         # if batch list inputs AND multiple samples per input, convert generated_id to 3D view
-        num_samples = generation_config.num_return_sequences or 1
-
+        num_samples = inference_kwargs.get("num_return_sequences", 1)
         if num_samples > 1 and isinstance(prompts, list):
             batch_size = input_ids.size(0)
-            num_return_sequences = generation_config.num_return_sequences or 1
-            generated_ids = generated_ids.view(batch_size, num_return_sequences, -1)
+            generated_ids = generated_ids.view(batch_size, num_samples, -1)
 
         return generated_ids
 
@@ -383,76 +278,41 @@ class Transformers:
             )
 
 
-def transformers(
-    model_name: str,
-    device: Optional[str] = None,
-    model_kwargs: dict = {},
-    tokenizer_kwargs: dict = {},
-    model_class=None,
-    tokenizer_class=None,
-):
-    """Instantiate a model from the `transformers` library and its tokenizer.
+class Mamba(Transformers):
+    """Represents a Mamba model."""
 
-    Parameters
-    ----------
-    model_name
-        The name of the model as listed on Hugging Face's model page.
-    device
-        The device(s) on which the model should be loaded. This overrides
-        the `device_map` entry in `model_kwargs` when provided.
-    model_kwargs
-        A dictionary that contains the keyword arguments to pass to the
-        `from_pretrained` method when loading the model.
-    tokenizer_kwargs
-        A dictionary that contains the keyword arguments to pass to the
-        `from_pretrained` method when loading the tokenizer.
+    def __init__(
+        self,
+        model_name: str,
+        model_kwargs: dict = {},
+        tokenizer_kwargs: dict = {},
+    ):
+        """
+        Create a Mamba model instance
 
-    Returns
-    -------
-    A `TransformersModel` model instance.
-
-    """
-    if model_class is None or tokenizer_class is None:
+        Parameters:
+        ----------
+        model_name
+            The name of the transformers model to use. It will be passed to
+            the `from_pretrained` method of the `MambaForCausalLM` class.
+        model_kwargs
+            A dictionary of keyword arguments to pass to the `from_pretrained`
+            method of the `MambaForCausalLM` class.
+        tokenizer_kwargs
+            A dictionary of keyword arguments to pass to the `from_pretrained`
+            method of the `AutoTokenizer` class.
+        """
         try:
-            from transformers import AutoModelForCausalLM, AutoTokenizer
+            from transformers import MambaForCausalLM
+
         except ImportError:
             raise ImportError(
-                "The `transformers` library needs to be installed in order to use `transformers` models."
+                "The `mamba_ssm`, `torch` and `transformer` libraries needs to be installed in order to use Mamba."
             )
-    if model_class is None:
-        model_class = AutoModelForCausalLM
-    if tokenizer_class is None:
-        tokenizer_class = AutoTokenizer
 
-    if device is not None:
-        model_kwargs["device_map"] = device
-
-    model = model_class.from_pretrained(model_name, **model_kwargs)
-
-    tokenizer_kwargs.setdefault("padding_side", "left")
-    tokenizer = tokenizer_class.from_pretrained(model_name, **tokenizer_kwargs)
-
-    return Transformers(model, tokenizer)
-
-
-def mamba(
-    model_name: str,
-    device: Optional[str] = None,
-    model_kwargs: dict = {},
-    tokenizer_kwargs: dict = {},
-):
-    try:
-        from transformers import MambaForCausalLM
-
-    except ImportError:
-        raise ImportError(
-            "The `mamba_ssm`, `torch` and `transformer` libraries needs to be installed in order to use Mamba."
+        return super().__init__(
+            model_name=model_name,
+            model_class=MambaForCausalLM,
+            model_kwargs=model_kwargs,
+            tokenizer_kwargs=tokenizer_kwargs,
         )
-
-    return transformers(
-        model_name=model_name,
-        device=device,
-        model_kwargs=model_kwargs,
-        tokenizer_kwargs=tokenizer_kwargs,
-        model_class=MambaForCausalLM,
-    )
