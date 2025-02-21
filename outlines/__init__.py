6b1bf705406a2c8898b5b712847202838926ab97
Rémi Louf <remilouf@protonmail.com>
2025-02-21 17:12:55 +0100

    Initialize `Anthropic` from client instance

--- outlines/__init__.py
+++ outlines/__init__.py
@@ -10,13 +10,16 @@ from outlines.caching import clear_cache, disable_cache, get_cache
 from outlines.function import Function
 from outlines.templates import Template, prompt
 
-from outlines.models import from_openai, from_gemini
+from outlines.models import from_openai, from_gemini, from_anthropic
 
 __all__ = [
     "clear_cache",
     "disable_cache",
     "get_cache",
     "Function",
+    "from_anthropic",
+    "from_gemini",
+    "from_openai",
     "prompt",
     "Template",
     "vectorize",
