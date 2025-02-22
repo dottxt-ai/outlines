123ca463bd2fffcb7cd5c6ce3bab1bb54ab21c27
Rémi Louf <remilouf@protonmail.com>
2025-02-22 15:09:02 +0100

    Update the README

--- outlines/__init__.py
+++ outlines/__init__.py
@@ -5,6 +5,7 @@ import outlines.grammars
 import outlines.models
 import outlines.processors
 import outlines.types
+from outlines.types import Choice, Regex, JsonType
 from outlines.base import vectorize
 from outlines.caching import clear_cache, disable_cache, get_cache
 from outlines.function import Function
