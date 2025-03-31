fb68b2b05e180204352f07c07a583de4d55a7f2c
Robin Picard <robin.picard@sciencespo.fr>
2025-03-31 15:43:42 +0200

    Restore the generate functions

---
+++ outlines/v0_legacy/generate/__init__.py
@@ -0,0 +1,17 @@
+from .cfg import cfg
+from .choice import choice
+from .format import format
+from .fsm import fsm
+from .json import json
+from .regex import regex
+from .text import text
+
+__all__ = [
+    "cfg",
+    "choice",
+    "format",
+    "fsm",
+    "json",
+    "regex",
+    "text",
+]
