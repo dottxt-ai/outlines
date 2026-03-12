4e52c917b4f4dc78ee2b4c1350abccd8569bee67
Rolando <38510643+RoloPineda@users.noreply.github.com>
2026-03-12 11:51:53 -0700

    style: fix ruff formatting

--- outlines/backends/outlines_core.py
+++ outlines/backends/outlines_core.py
@@ -12,16 +12,14 @@ from outlines.models.mlxlm import MLXLM
 from outlines.models.transformers import Transformers
 from outlines.processors.base_logits_processor import (
     OutlinesLogitsProcessor,
-    TensorType
+    TensorType,
 )
 
 
 class OutlinesCoreLogitsProcessor(OutlinesLogitsProcessor):
     """Logits processor for Outlines Core."""
 
-    def __init__(
-        self, index: Index, tensor_library_name: str
-    ):
+    def __init__(self, index: Index, tensor_library_name: str):
         """
         Parameters
         ----------
@@ -69,91 +67,77 @@ class OutlinesCoreLogitsProcessor(OutlinesLogitsProcessor):
             self.allocate_token_bitmask = allocate_token_bitmask
             self.bias_logits = self._bias_logits_numpy
 
-        elif self.tensor_library_name == "mlx": # pragma: no cover
-            from outlines_core.kernels.mlx import (
-                allocate_token_bitmask
-            )
+        elif self.tensor_library_name == "mlx":  # pragma: no cover
+            from outlines_core.kernels.mlx import allocate_token_bitmask
 
             self.allocate_token_bitmask = allocate_token_bitmask
             self.bias_logits = self._bias_logits_mlx
 
-        else: # pragma: no cover
-            raise ValueError(
-                f"Unsupported tensor library: {self.tensor_library_name}"
-            )
+        else:  # pragma: no cover
+            raise ValueError(f"Unsupported tensor library: {self.tensor_library_name}")
 
         self._guides = [Guide(self.index) for _ in range(batch_size)]
         self._bitmasks = [
-            self.allocate_token_bitmask(vocab_size)
-            for _ in range(batch_size)
+            self.allocate_token_bitmask(vocab_size) for _ in range(batch_size)
         ]
 
-    def _bias_logits_mlx( # pragma: no cover
+    def _bias_logits_mlx(  # pragma: no cover
         self, batch_size: int, logits: TensorType
     ) -> TensorType:
         """Bias the logits for MLX tensors."""
         from outlines_core.kernels.mlx import (
             apply_token_bitmask,
-            fill_next_token_bitmask
+            fill_next_token_bitmask,
         )
 
         biased_logits_array = []
         for i in range(batch_size):
             fill_next_token_bitmask(self._guides[i], self._bitmasks[i])
             biased_logits = apply_token_bitmask(
-                self.tensor_adapter.unsqueeze(logits[i]), self._bitmasks[i] # type: ignore
+                self.tensor_adapter.unsqueeze(logits[i]),
+                self._bitmasks[i],  # type: ignore
             )
             biased_logits_array.append(biased_logits)
 
         return self.tensor_adapter.concatenate(biased_logits_array)
 
-    def _bias_logits_torch(
-        self, batch_size: int, logits: TensorType
-    ) -> TensorType:
+    def _bias_logits_torch(self, batch_size: int, logits: TensorType) -> TensorType:
         """Bias the logits for Torch tensors."""
         from outlines_core.kernels.torch import (
             apply_token_bitmask_inplace,
-            fill_next_token_bitmask
+            fill_next_token_bitmask,
         )
 
         for i in range(batch_size):
             fill_next_token_bitmask(self._guides[i], self._bitmasks[i])
             self._bitmasks[i] = self.tensor_adapter.to_device(
-                self._bitmasks[i],
-                self.tensor_adapter.get_device(logits)
+                self._bitmasks[i], self.tensor_adapter.get_device(logits)
             )
             apply_token_bitmask_inplace(
-                self.tensor_adapter.unsqueeze(logits[i]), # type: ignore
-                self._bitmasks[i]
-            )
-            self._bitmasks[i] = self.tensor_adapter.to_device(
+                self.tensor_adapter.unsqueeze(logits[i]),  # type: ignore
                 self._bitmasks[i],
-                "cpu"
             )
+            self._bitmasks[i] = self.tensor_adapter.to_device(self._bitmasks[i], "cpu")
 
         return logits
 
-    def _bias_logits_numpy(
-        self, batch_size: int, logits: TensorType
-    ) -> TensorType:
+    def _bias_logits_numpy(self, batch_size: int, logits: TensorType) -> TensorType:
         """Bias the logits for Numpy tensors."""
         from outlines_core.kernels.numpy import (
             apply_token_bitmask_inplace,
-            fill_next_token_bitmask
+            fill_next_token_bitmask,
         )
 
         for i in range(batch_size):
             fill_next_token_bitmask(self._guides[i], self._bitmasks[i])
             apply_token_bitmask_inplace(
-                self.tensor_adapter.unsqueeze(logits[i]), # type: ignore
-                self._bitmasks[i]
+                self.tensor_adapter.unsqueeze(logits[i]),  # type: ignore
+                self._bitmasks[i],
             )
 
         return logits
 
-    def process_logits(
-        self, input_ids: TensorType, logits: TensorType
-    ) -> TensorType:
+    def process_logits(self, input_ids: TensorType, logits: TensorType) -> TensorType:
         """Use the guides to bias the logits.
 
         Parameters
@@ -177,18 +161,14 @@ class OutlinesCoreLogitsProcessor(OutlinesLogitsProcessor):
             self.is_first_token = False
         else:
             for i in range(batch_size):
-                last_token_id = self.tensor_adapter.to_scalar(input_ids[i][-1]) # type: ignore
+                last_token_id = self.tensor_adapter.to_scalar(input_ids[i][-1])  # type: ignore
                 # This circumvents issue #227 in outlines_core
                 # Ideally, we would be able to advance all the times as the final
                 # state would accept the eos token leading to itself
-                if (
-                    not self._guides[i].is_finished()
-                    or self._guides[i].accepts_tokens([last_token_id])
+                if not self._guides[i].is_finished() or self._guides[i].accepts_tokens(
+                    [last_token_id]
                 ):
-                    self._guides[i].advance(
-                        token_id=last_token_id,
-                        return_tokens=False
-                    )
+                    self._guides[i].advance(token_id=last_token_id, return_tokens=False)
 
         return self.bias_logits(batch_size, logits)
 
@@ -211,18 +191,18 @@ class OutlinesCoreBackend(BaseBackend):
             eos_token = tokenizer.eos_token
             token_to_str = tokenizer.convert_token_to_string
         elif isinstance(model, LlamaCpp):
-            tokenizer = model.tokenizer # type: ignore
+            tokenizer = model.tokenizer  # type: ignore
             vocabulary = tokenizer.vocabulary
             eos_token_id = tokenizer.eos_token_id
             eos_token = tokenizer.eos_token
             token_to_str = tokenizer.convert_token_to_string
-        elif isinstance(model, MLXLM): # pragma: no cover
-            tokenizer = model.mlx_tokenizer # type: ignore
+        elif isinstance(model, MLXLM):  # pragma: no cover
+            tokenizer = model.mlx_tokenizer  # type: ignore
             vocabulary = tokenizer.get_vocab()
             eos_token_id = tokenizer.eos_token_id
             eos_token = tokenizer.eos_token
-            token_to_str = lambda token: tokenizer.convert_tokens_to_string([token]) # type: ignore
-        else: # pragma: no cover
+            token_to_str = lambda token: tokenizer.convert_tokens_to_string([token])  # type: ignore
+        else:  # pragma: no cover
             raise ValueError(f"Unsupported model type: {type(model)}")
 
         self.eos_token_id = eos_token_id
@@ -231,9 +211,7 @@ class OutlinesCoreBackend(BaseBackend):
         )
         self.tensor_library_name = model.tensor_library_name
 
-    def get_json_schema_logits_processor(
-        self, json_schema: str
-    ):
+    def get_json_schema_logits_processor(self, json_schema: str):
         """Create a logits processor from a JSON schema.
 
         Parameters
@@ -277,7 +255,7 @@ class OutlinesCoreBackend(BaseBackend):
         vocab: Dict[str, int],
         eos_token_id: int,
         eos_token: str,
-        token_to_str: Callable[[str], str]
+        token_to_str: Callable[[str], str],
     ) -> Vocabulary:
         """Create an Outlines Core Vocabulary instance.
 
