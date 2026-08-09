4965c6aac872693040e91f5a3104b3bded2c4d6e
Goutham Annem <gouthemannem@gmail.com>
2026-08-09 08:03:52 -0700

    fix(fr): extend postal_code to cover overseas departments (97xxx/98xxx)
    Change 9[0-5]\d{3} → 9[0-8]\d{3} so that DOM-TOM codes (Guadeloupe 971xx,
    Martinique 972xx, Guyane 973xx, La Réunion 974xx, Mayotte 976xx, Monaco
    980xx, French Polynesia 987xx, New Caledonia 988xx) are valid outputs.
    99xxx remains excluded.
    
    Addresses review feedback from ErenAta16 and RobinPicard.
    
    Signed-off-by: Goutham Annem <gouthemannem@gmail.com>

--- src/outlines/types/locale/fr.py
+++ src/outlines/types/locale/fr.py
@@ -3,4 +3,4 @@
 from outlines.types.dsl import Regex
 
 phone_number = Regex(r"(\+33\s?|0)[1-9](\s?\d{2}){4}")
-postal_code = Regex(r"0[1-9]\d{3}|[1-8]\d{4}|9[0-5]\d{3}")
+postal_code = Regex(r"0[1-9]\d{3}|[1-8]\d{4}|9[0-8]\d{3}")
